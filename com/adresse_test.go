648096f714b9cac4fd5cf2dfc919e3966c9aa174
konstantin <konstantin.klein@hochfrequenz.de>
2021-07-23 16:15:36 +0200

    wip
    test

--- com/adresse_test.go
+++ com/adresse_test.go
@@ -1,11 +1,11 @@
 package com
 
 import (
-	"testing"
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"testing"
 )
 
 type Suite struct {
@@ -15,8 +15,17 @@ type Suite struct {
 // SetupSuite sets up the tests
 func (s *Suite) SetupSuite() {
 }
+
 // Test_Deserialization deserializes an address json
 func (s *Suite) Test_Deserialization() {
+	_ = Adresse{
+		Postleitzahl: "82031",
+		Ort:          "Grünwald",
+		Strasse:      "Nördlicher Münchner Straße",
+		Hausnummer:   "27A",
+		Landescode:   Landescode.DE,
+	}
+	then.Assert
 	then.AssertThat(s.T(), err, is.Not(is.Nil()))
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/hochfrequenz/go-bo4e
 
 go 1.16
+
+require (
+	github.com/corbym/gocrest v1.0.5
+	github.com/stretchr/testify v1.7.0
+// github.com/hochfrequenz/go-bo4e latest
+)
