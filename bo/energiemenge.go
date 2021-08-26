5b41bf4ca027cf2a0110a2201cf2a8261510817c
konstantin <konstantin.klein@hochfrequenz.de>
2021-08-26 16:54:05 +0200

    implement BO4E.BO.Energiemenge

---
+++ bo/energiemenge.go
@@ -0,0 +1,14 @@
+package bo
+
+import (
+	"github.com/hochfrequenz/go-bo4e/com"
+	"github.com/hochfrequenz/go-bo4e/enum/lokationstyp"
+)
+
+// Energiemenge contains information about consumption at a location
+type Energiemenge struct {
+	BusinessObject
+	LokationsId  string                    `json:"lokationsId" example:"DE0123456789012345678901234567890" validate:"alphanum,required,min=11,max=33"` // ID of the location (either a LokationsTyp MaLo ID (11 digits) or a LokationsTyp MeLo ID (33 alphanum))
+	LokationsTyp lokationstyp.Lokationstyp `json:"lokationsTyp" example:"MeLo" validate:"required"`                                                    // type of the location in LokationsId
+	Verbrauch    []com.Verbrauch           `json:"energieverbrauch" validate:"required,min=1"`                                                         // consumption data (see Verbrauch)
+}
---
+++ bo/energiemenge_test.go
@@ -0,0 +1,89 @@
+package bo
+
+import (
+	"github.com/go-playground/validator"
+	"github.com/hochfrequenz/go-bo4e/com"
+	"github.com/hochfrequenz/go-bo4e/enum/botyp"
+	"github.com/hochfrequenz/go-bo4e/enum/lokationstyp"
+	"github.com/hochfrequenz/go-bo4e/enum/mengeneinheit"
+	"github.com/hochfrequenz/go-bo4e/enum/wertermittlungsverfahren"
+	"time"
+)
+
+// TestFailedEnergiemengeValidation verifies that the validators of Energiemenge work
+func (s *Suite) TestFailedEnergiemengeValidation() {
+	var verbrauch = com.Verbrauch{
+		Startdatum:               time.Now(),
+		Enddatum:                 time.Now().Add(time.Minute * 15),
+		Wertermittlungsverfahren: wertermittlungsverfahren.MESSUNG,
+		Obiskennzahl:             "1-0:1.8.1",
+		Wert:                     17,
+		Einheit:                  mengeneinheit.KWH,
+	}
+	validate := validator.New()
+	invalidMesslokationMap := map[string][]interface{}{
+		"required": {
+			Energiemenge{
+				BusinessObject: BusinessObject{
+					BoTyp:             0,
+					VersionStruktur:   "",
+					ExterneReferenzen: nil,
+				},
+				LokationsId:  "",
+				LokationsTyp: 0,
+				Verbrauch:    nil,
+			},
+		},
+		"min": {
+			Energiemenge{
+				BusinessObject: BusinessObject{
+					BoTyp:             botyp.Energiemenge,
+					VersionStruktur:   "1",
+					ExterneReferenzen: nil,
+				},
+				LokationsId:  "not11len",
+				LokationsTyp: lokationstyp.MeLo,
+				Verbrauch:    []com.Verbrauch{verbrauch},
+			},
+		},
+		"alphanum": {
+			Energiemenge{
+				BusinessObject: BusinessObject{
+					BoTyp:             botyp.Energiemenge,
+					VersionStruktur:   "1",
+					ExterneReferenzen: nil,
+				},
+				LokationsId:  "not alpha numeric",
+				LokationsTyp: lokationstyp.MeLo,
+				Verbrauch:    []com.Verbrauch{verbrauch},
+			},
+		},
+	}
+	VerfiyFailedValidations(s, validate, invalidMesslokationMap)
+}
+
+//  TestSuccessfulMesslokationValidation verifies that a valid BO is validated without errors
+func (s *Suite) TestSuccessfulEnergiemengeValidation() {
+	validate := validator.New()
+	var verbrauch = com.Verbrauch{
+		Startdatum:               time.Now(),
+		Enddatum:                 time.Now().Add(time.Minute * 15),
+		Wertermittlungsverfahren: wertermittlungsverfahren.MESSUNG,
+		Obiskennzahl:             "1-0:1.8.1",
+		Wert:                     17,
+		Einheit:                  mengeneinheit.KWH,
+	}
+	validEnergiemengen := []interface{}{
+		Energiemenge{
+			BusinessObject: BusinessObject{
+				BoTyp:             botyp.Energiemenge,
+				VersionStruktur:   "1",
+				ExterneReferenzen: nil,
+			},
+			LokationsId:  "54321098765",
+			LokationsTyp: lokationstyp.MeLo,
+			Verbrauch:    []com.Verbrauch{verbrauch},
+		},
+	}
+	VerfiySuccessfulValidations(s, validate, validEnergiemengen)
+}
