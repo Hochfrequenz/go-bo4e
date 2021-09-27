db57b345fdc6cf02d15e1f5c41fac935a76cf52f
konstantin <konstantin.klein@hochfrequenz.de>
2021-09-27 08:33:41 +0200

    fix wrong docstrings in zaehlertyp

--- enum/zaehlertyp/zahlertyp.go
+++ enum/zaehlertyp/zahlertyp.go
@@ -1,18 +1,19 @@
 package zaehlertyp
 
-// Sparte contains different divisions of typical utilities.
+// Zaehlertyp contains different types of meters for power and gas
 //go:generate stringer --type Zaehlertyp
 //go:generate jsonenums --type Zaehlertyp
 type Zaehlertyp int
 
 const (
-	Drehstromzaehler      Zaehlertyp = iota + 1 // Drehstromzähler
-	Balgengaszaehler                            //	Balgengaszähler
-	Drehkolbenzaehler                           //	Drehkolbengaszähler
-	SmartMeter                                  // Smart Meter Zähler
-	Leistungszaehler                            //	leistungsmessender Zähler
-	Maximumgaehler                              //	Maximumzähler
-	Turbinenradgaszaehler                       //	Turbinenradgaszähler
-	Ultraschallgaszaehler                       //	Ultraschallgaszähler
-	Wechselstromzaehler                         // Wechselstromzähler
+	// Drehstromzaehler is a polyphase meter
+	Drehstromzaehler      Zaehlertyp = iota + 1
+	Balgengaszaehler                 // Balgengaszähler
+	Drehkolbenzaehler                // Drehkolbengaszähler
+	SmartMeter                       // Smart Meter Zähler
+	Leistungszaehler                 // leistungsmessender Zähler
+	Maximumgaehler                   // Maximumzähler
+	Turbinenradgaszaehler            // Turbinenradgaszähler
+	Ultraschallgaszaehler            // Ultraschallgaszähler
+	Wechselstromzaehler              // Wechselstromzähler
 )
