565811d97e078aa18af5ab326b67fed554bdb682
konstantin <konstantin.klein@hochfrequenz.de>
2021-09-27 11:00:41 +0200

    implement com betrag
    naming
    
    
    fix docstring

--- com/betrag.go
+++ com/betrag.go
@@ -1,31 +1,11 @@
 package com
 
 import (
-	"github.com/go-playground/validator"
-	"github.com/hochfrequenz/go-bo4e/enum/landescode"
+	"github.com/hochfrequenz/go-bo4e/enum/waehrungscode"
 )
 
-// Adresse is a general purpose address struct
-type Adresse struct {
-	Postleitzahl string                `json:"postleitzahl,omitempty" example:"82031" validate:"numeric,required"` // zip code, mandatory
-	Ort          string                `json:"ort,omitempty" example:"Grünwald" validate:"alphaunicode,required"`  // city name, mandatory
-	Strasse      string                `json:"strasse,omitempty" example:"Nördliche Münchner Straße 27A"`          // street name
-	Hausnummer   string                `json:"hausnummer,omitempty" example:"27A"`                                 // house number including suffixes like "a" or "-1"
-	Postfach     string                `json:"postfach,omitempty"`                                                 // post box
-	Adresszusatz string                `json:"adresszusatz,omitempty" example:"3. Stock linke Wohnung"`            // additional data, like f.e. floor or "Hinterhaus"
-	CoErgaenzung string                `json:"co_ergaenzung,omitempty" example:"c/o Veronica Hauptmieterin"`       //
-	Landescode   landescode.Landescode `json:"landescode,omitempty"`                                               // ISO 3166 country code
-}
-
-// AdresseStructLevelValidation does a cross check on a Adresse object
-func AdresseStructLevelValidation(sl validator.StructLevel) {
-	// ToDo: use required_without/required_if instead of own validator
-	// see https://github.com/go-playground/validator/search?q=required_without
-	address := sl.Current().Interface().(Adresse)
-	if (len(address.Strasse) == 0 && len(address.Postfach) == 0) || (len(address.Strasse) > 0 && len(address.Postfach) > 0) {
-		sl.ReportError(address.Strasse, "Strasse", "Postfach", "StrasseXORPostfach", "")
-	}
-	if (len(address.Strasse) > 0 && len(address.Hausnummer) == 0) || (len(address.Strasse) == 0 && len(address.Hausnummer) > 0) {
-		sl.ReportError(address.Strasse, "Strasse", "Hausnummer", "StrasseRequiresHausnummer", "")
-	}
+// Betrag wird dazu verwendet, Summenbeträge - beispielsweise in Angeboten und Rechnungen - als Geldbeträge abzubilden. Die Einheit ist dabei immer die Hauptwährung also Euro, Dollar etc
+type Betrag struct {
+	Wert     float32                     `json:"wert" example:"18.36" validate:"required"`   // amount
+	Waehrung waehrungscode.Waehrungscode `json:"waehrung" example:"EUR" validate:"required"` // currency
 }
