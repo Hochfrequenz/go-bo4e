184692c74506d56d7af37e2bc7ae6c669e06d929
konstantin <konstantin.klein@hochfrequenz.de>
2021-09-27 08:14:19 +0200

    test zaehlerstaende functions

---
+++ com/zaehlerstaende_test.go
@@ -0,0 +1,82 @@
+package com
+
+import (
+	"github.com/corbym/gocrest/is"
+	"github.com/corbym/gocrest/then"
+	"github.com/hochfrequenz/go-bo4e/enum/mengeneinheit"
+	"github.com/hochfrequenz/go-bo4e/enum/wertermittlungsverfahren"
+	"time"
+)
+
+// TestZaehlerstaneLen tests the Zaehlerstaende.Len function
+func (s *Suite) TestZaehlerstandeLen() {
+	zaehlerstaende := Zaehlerstaende{
+		Zaehlerstand{},
+		Zaehlerstand{},
+		Zaehlerstand{},
+	}
+	then.AssertThat(s.T(), zaehlerstaende.Len(), is.EqualTo(3))
+}
+
+//  Test_Zaehlerstande_Swap tests the Zaehlerstaende.Swap function
+func (s *Suite) Test_Zaehlerstande_Swap() {
+	zaehlerstaende := Zaehlerstaende{
+		Zaehlerstand{
+			Ablesedatum:              time.Time{},
+			Wertermittlungsverfahren: wertermittlungsverfahren.MESSUNG,
+			Wert:                     17.0,
+			Einheit:                  mengeneinheit.KW,
+		},
+		Zaehlerstand{
+			Ablesedatum:              time.Time{},
+			Wertermittlungsverfahren: wertermittlungsverfahren.MESSUNG,
+			Wert:                     42.1,
+			Einheit:                  mengeneinheit.KW,
+		},
+		Zaehlerstand{
+			Ablesedatum:              time.Time{},
+			Wertermittlungsverfahren: wertermittlungsverfahren.MESSUNG,
+			Wert:                     123.2,
+			Einheit:                  mengeneinheit.KW,
+		},
+	}
+	then.AssertThat(s.T(), zaehlerstaende[1].Wert, is.EqualTo(float32(42.1)))
+	then.AssertThat(s.T(), zaehlerstaende[2].Wert, is.EqualTo(float32(123.2)))
+	zaehlerstaende.Swap(1, 2)
+	then.AssertThat(s.T(), zaehlerstaende[1].Wert, is.EqualTo(float32(123.2)))
+	then.AssertThat(s.T(), zaehlerstaende[2].Wert, is.EqualTo(float32(42.1)))
+}
+
+//  Test_Zaehlerstaende_Less tests the Zaehlerstaende.Less function
+func (s *Suite) Test_Zaehlerstaende_Less() {
+	zaehlerstaende := Zaehlerstaende{
+		Zaehlerstand{
+			Ablesedatum:              time.Date(2025, 8, 1, 0, 0, 0, 0, time.UTC),
+			Wertermittlungsverfahren: wertermittlungsverfahren.MESSUNG,
+			Wert:                     17,
+			Einheit:                  mengeneinheit.KW,
+		},
+		Zaehlerstand{
+			Ablesedatum:              time.Date(2045, 8, 1, 0, 0, 0, 0, time.UTC),
+			Wertermittlungsverfahren: wertermittlungsverfahren.MESSUNG,
+			Wert:                     123,
+			Einheit:                  mengeneinheit.KW,
+		},
+		Zaehlerstand{
+			Ablesedatum:              time.Date(2038, 8, 1, 0, 0, 0, 0, time.UTC),
+			Wertermittlungsverfahren: wertermittlungsverfahren.MESSUNG,
+			Wert:                     42,
+			Einheit:                  mengeneinheit.KW,
+		},
+		Zaehlerstand{
+			Ablesedatum:              time.Date(2025, 8, 1, 0, 0, 0, 0, time.UTC),
+			Wertermittlungsverfahren: wertermittlungsverfahren.MESSUNG,
+			Wert:                     18,
+			Einheit:                  mengeneinheit.KW,
+		},
+	}
+	then.AssertThat(s.T(), zaehlerstaende.Less(1, 2), is.True())
+	then.AssertThat(s.T(), zaehlerstaende.Less(2, 1), is.False())
+	then.AssertThat(s.T(), zaehlerstaende.Less(3, 0), is.False())
+	then.AssertThat(s.T(), zaehlerstaende.Less(0, 3), is.False())
+}
