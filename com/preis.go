529b53559625b3843e12d7cd667d83ba89e4ea6f
konstantin <konstantin.klein@hochfrequenz.de>
2021-09-27 12:19:36 +0200

    implement COM Preis

---
+++ com/preis.go
@@ -0,0 +1,15 @@
+package com
+
+import (
+	"github.com/hochfrequenz/go-bo4e/enum/mengeneinheit"
+	"github.com/hochfrequenz/go-bo4e/enum/preisstatus"
+	"github.com/hochfrequenz/go-bo4e/enum/waehrungseinheit"
+)
+
+// Preis is a price
+type Preis struct {
+	Wert       float32                           `json:"wert" validate:"required"`       // Gibt die nominale Höhe des Preises an
+	Einheit    waehrungseinheit.Waehrungseinheit `json:"einheit" validate:"required"`    // Währungseinheit für den Preis
+	Bezugswert mengeneinheit.Mengeneinheit       `json:"bezugswert" validate:"required"` // Angabe, für welche Bezugsgröße der Preis gilt. Z.B. kWh
+	Status     preisstatus.Preisstatus           `json:"status"`                         // Gibt den Status des veröffentlichten Preises an, Details siehe ENUM Preisstatus
+}
