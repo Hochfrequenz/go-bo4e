bd395fedad6000e8eaab299d8f7af8bf3557e2a6
konstantin <konstantin.klein@hochfrequenz.de>
2021-09-27 08:30:47 +0200

    fix wrong docstrigns of zaehlerauspraegung

--- enum/zaehlerauspraegung/zaehlerauspraegung.go
+++ enum/zaehlerauspraegung/zaehlerauspraegung.go
@@ -6,6 +6,8 @@ package zaehlerauspraegung
 type Zaehlerauspraegung int
 
 const (
-	Einrichtungszaehler  Zaehlerauspraegung = iota + 1 // power/electricity
-	Zweirichtungszaehler                               // gas
+	// Einrichtungszaehler is a single direction meter
+	Einrichtungszaehler Zaehlerauspraegung = iota + 1
+	// Zweirichtungszaehler is a bidirectional meter
+	Zweirichtungszaehler
 )
