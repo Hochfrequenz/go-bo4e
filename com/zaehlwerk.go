f2a2197158d55f8536577e3a0d034554838effbf
konstantin <konstantin.klein@hochfrequenz.de>
2021-09-27 08:17:01 +0200

    make zaehlwerk bezeichnung required
    see https://www.bo4e.de/dokumentation/komponenten/com-zahlwerk

--- com/zaehlwerk.go
+++ com/zaehlwerk.go
@@ -8,9 +8,9 @@ import (
 // A Zaehlwerk is the counting part of a meter. A meter consists of one or more Zaehlwerke
 type Zaehlwerk struct {
 	ZaehlwerkId    string                          `json:"zaehlwerkId" validate:"required"`  // Identifikation des Zählwerks (Registers) innerhalb des Zählers. Oftmals eine laufende Nummer hinter der Zählernummer. Z.B. 47110815_1
-	Bezeichnung    string                          `json:"bezeichnung"`                      // Zusätzliche Bezeichnung, z.B. Zählwerk_Wirkarbeit.
+	Bezeichnung    string                          `json:"bezeichnung" validate:"required"`  // Zusätzliche Bezeichnung, z.B. Zählwerk_Wirkarbeit.
 	Richtung       energierichtung.Energierichtung `json:"richtung" validate:"required"`     // Die Energierichtung, Einspeisung oder Ausspeisung. Details siehe ENUM Energierichtung
-	ObisKennzahl   string                          `json:"obisKennzahl" validate:"required"` //Die OBIS-Kennzahl für das Zählwerk, die festlegt, welche auf die gemessene Größe mit dem Stand gemeldet wird. Nur Zählwerkstände mit dieser OBIS-Kennzahl werden an diesem Zählwerk registriert. Beispiel: 1-0:1.8.1 für elektrische Wirkarbeit.
+	ObisKennzahl   string                          `json:"obisKennzahl" validate:"required"` // Die OBIS-Kennzahl für das Zählwerk, die festlegt, welche auf die gemessene Größe mit dem Stand gemeldet wird. Nur Zählwerkstände mit dieser OBIS-Kennzahl werden an diesem Zählwerk registriert. Beispiel: 1-0:1.8.1 für elektrische Wirkarbeit.
 	Wandlerfaktor  float32                         `json:"wandlerfaktor"`                    // Mit diesem Faktor wird eine Zählerstandsdifferenz multipliziert, um zum eigentlichen Verbrauch im Zeitraum zu kommen.
 	Einheit        mengeneinheit.Mengeneinheit     `json:"einheit" validate:"required"`      // Die Einheit der gemessenen Größe, z.B. kWh.
 	Zaehlerstaende Zaehlerstaende                  `json:"zaehlerstaende,omitempty"`         // Non BO4E Standard
