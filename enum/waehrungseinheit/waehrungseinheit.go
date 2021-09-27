a0d0a981e3c75500d9cd1a61ba73f35290543345
konstantin <konstantin.klein@hochfrequenz.de>
2021-09-27 12:13:28 +0200

    implement ENUM waehrungseinheit

---
+++ enum/waehrungseinheit/waehrungseinheit.go
@@ -0,0 +1,13 @@
+package waehrungseinheit
+
+// why tf is this a separate enum and not part of the waehrungscode.Waehrungscode?
+
+// Waehrungseinheit models euro and euro cent. In diesem Enum werden die Währungen und ihre Untereinheiten definiert, beispielsweise für die Verwendung in Preisen.
+//go:generate stringer --type Waehrungseinheit
+//go:generate jsonenums --type Waehrungseinheit
+type Waehrungseinheit int
+
+const (
+	EUR Waehrungseinheit = iota + 1 // Euro
+	CT                              // Eurocent
+)
