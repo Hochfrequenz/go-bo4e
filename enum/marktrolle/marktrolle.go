7fd367fcee3cb6ad774925b96c13126b282df8ca
konstantin <konstantin.klein@hochfrequenz.de>
2021-10-05 16:35:52 +0200

    implement ENUM marktrolle

---
+++ enum/marktrolle/marktrolle.go
@@ -0,0 +1,23 @@
+package marktrolle
+
+// The Marktrolle describes the roles of a Marktteilnehmer
+//go:generate stringer --type Marktrolle
+//go:generate jsonenums --type Marktrolle
+type Marktrolle int
+
+const (
+	NB              Marktrolle = iota + 1 // NB is a Netzbetreiber
+	LF                                    // LF is a Lieferant
+	MSB                                   // MSB is a Messstellenbetreiber
+	MDL                                   // MDL is a Messdienstleister
+	DL                                    // DL is a Dienstleister
+	BKV                                   // BKV is a Bilanzkreisverantwortlicher
+	BKO                                   // BKO is a Bilanzkoordinator/Marktgebietsverantwortlicher
+	UENB                                  // UENB is a Übertragungsnetzbetreiber
+	KUNDE_SELBST_NN                       // KUNDE_SELBST_NN is a Kunden die NN-Entgelte selbst zahlen
+	MGV                                   // MGV is a Marktgebietsverantwortlicher
+	EIV                                   // EIV is a Einsatzverantwortlicher
+	RB                                    // RB is a Registerbetreiber Beispielsweise Herkunftsnachweisregister
+	KUNDE                                 // KUNDE is a Kunde
+	INTERESSENT                           // INTERESSENT is a Interessent
+)
