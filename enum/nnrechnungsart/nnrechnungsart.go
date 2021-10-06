e9d86139918e17df1b91d6a44b8b6fabac9ab8a9
konstantin <konstantin.klein@hochfrequenz.de>
2021-10-06 08:30:50 +0200

    implement enum nnrechnungsart
    casing

---
+++ enum/nnrechnungsart/nnrechnungsart.go
@@ -0,0 +1,11 @@
+package nnrechnungsart
+
+// Nnrechnungsart ist die Art der bo.Netznutzungsrechnung (Abbildung verschiedener in der INVOIC angegebenen Rechnungsarten)
+//go:generate stringer --type NNRechnungsart
+//go:generate jsonenums --type NNRechnungsart
+type NNRechnungsart int
+
+const (
+	HANDELSRECHNUNG   NNRechnungsart = iota + 1 // Handelsrechnung
+	SELBSTAUSGESTELLT                           // Selbst ausgestellte Rechnung, z.B. für Einspeiserechnungen.
+)
