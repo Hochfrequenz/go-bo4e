eee44808a2c186f840de596d7a4d6b92c3e21e5c
konstantin <konstantin.klein@hochfrequenz.de>
2021-10-06 08:51:31 +0200

    fix code duplication

--- bo/netznutzungsrechnung_test.go
+++ bo/netznutzungsrechnung_test.go
@@ -7,20 +7,15 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/hochfrequenz/go-bo4e/com"
 	"github.com/hochfrequenz/go-bo4e/enum/anrede"
-	"github.com/hochfrequenz/go-bo4e/enum/bdewartikelnummer"
 	"github.com/hochfrequenz/go-bo4e/enum/botyp"
 	"github.com/hochfrequenz/go-bo4e/enum/geschaeftspartnerrolle"
 	"github.com/hochfrequenz/go-bo4e/enum/landescode"
-	"github.com/hochfrequenz/go-bo4e/enum/mengeneinheit"
 	"github.com/hochfrequenz/go-bo4e/enum/nnrechnungsart"
 	"github.com/hochfrequenz/go-bo4e/enum/nnrechnungstyp"
-	"github.com/hochfrequenz/go-bo4e/enum/preisstatus"
 	"github.com/hochfrequenz/go-bo4e/enum/rechnungsstatus"
 	"github.com/hochfrequenz/go-bo4e/enum/rechnungstyp"
 	"github.com/hochfrequenz/go-bo4e/enum/sparte"
-	"github.com/hochfrequenz/go-bo4e/enum/steuerkennzeichen"
 	"github.com/hochfrequenz/go-bo4e/enum/waehrungscode"
-	"github.com/hochfrequenz/go-bo4e/enum/waehrungseinheit"
 	"github.com/hochfrequenz/go-bo4e/enum/zeiteinheit"
 	"github.com/shopspring/decimal"
 	"time"
@@ -181,141 +176,7 @@ func (s *Suite) Test_Successful_Netznutzungsrechnung_Validation() {
 	validate := validator.New()
 	validNnrs := []interface{}{
 		Netznutzungsrechnung{
-			Rechnung: Rechnung{
-				BusinessObject: BusinessObject{
-					BoTyp:             botyp.Rechnung,
-					VersionStruktur:   "1",
-					ExterneReferenzen: nil,
-				},
-				Rechnungstitel:          "De",
-				Rechnungsstatus:         rechnungsstatus.Bezahlt,
-				Storno:                  true,
-				Rechnungsnummer:         "123",
-				Rechnungsdatum:          time.Date(2023, 8, 1, 0, 0, 0, 0, time.UTC),
-				Faelligkeitsdatum:       time.Date(2022, 8, 1, 0, 0, 0, 0, time.UTC),
-				Rechnungstyp:            rechnungstyp.Endkundenrechnung,
-				OriginalRechnungsnummer: "hallo",
-				Rechnungsperiode: com.Zeitraum{
-					Einheit:        zeiteinheit.Minute,
-					Dauer:          decimal.NullDecimal{Valid: true, Decimal: decimal.NewFromFloat(15)},
-					Startzeitpunkt: time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC),
-					Endzeitpunkt:   time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC).Add(time.Minute * 15),
-				},
-				Rechnungsersteller: Geschaeftspartner{
-					BusinessObject: BusinessObject{
-						BoTyp:             botyp.Geschaeftspartner,
-						VersionStruktur:   "1",
-						ExterneReferenzen: nil,
-					},
-					Anrede:               anrede.Divers,
-					Name1:                "Müller",
-					Name2:                "Lieschen",
-					Name3:                "",
-					Gewerbekennzeichnung: false,
-					HrNummer:             "handelsregister foo",
-					Amtsgericht:          "amtsgericht bar",
-					Kontaktweg:           0,
-					UmsatzsteuerId:       "umsatzsteuer foo",
-					GlaeubigerId:         "glauebiger bar",
-					EMailAdresse:         "email@lieschen-mueller.de",
-					Website:              "https://lieschen-mueller.de",
-					Geschaeftspartnerrollen: []geschaeftspartnerrolle.Geschaeftspartnerrolle{
-						geschaeftspartnerrolle.Kunde,
-						geschaeftspartnerrolle.Marktpartner,
-					},
-					Partneradresse: com.Adresse{
-						Postleitzahl: "82031",
-						Ort:          "Grünwald",
-						Strasse:      "Nördlicher Münchner Straße",
-						Hausnummer:   "27A",
-						Landescode:   landescode.DE,
-					},
-				},
-				Rechnungsempfaenger: Geschaeftspartner{
-					BusinessObject: BusinessObject{
-						BoTyp:             botyp.Geschaeftspartner,
-						VersionStruktur:   "1",
-						ExterneReferenzen: nil,
-					},
-					Anrede:               anrede.Divers,
-					Name1:                "Müller",
-					Name2:                "Lieschen",
-					Name3:                "",
-					Gewerbekennzeichnung: false,
-					HrNummer:             "handelsregister foo",
-					Amtsgericht:          "amtsgericht bar",
-					Kontaktweg:           0,
-					UmsatzsteuerId:       "umsatzsteuer foo",
-					GlaeubigerId:         "glauebiger bar",
-					EMailAdresse:         "email@lieschen-mueller.de",
-					Website:              "https://lieschen-mueller.de",
-					Geschaeftspartnerrollen: []geschaeftspartnerrolle.Geschaeftspartnerrolle{
-						geschaeftspartnerrolle.Kunde,
-						geschaeftspartnerrolle.Marktpartner,
-					},
-					Partneradresse: com.Adresse{
-						Postleitzahl: "82031",
-						Ort:          "Grünwald",
-						Strasse:      "Nördlicher Münchner Straße",
-						Hausnummer:   "27A",
-						Landescode:   landescode.DE,
-					},
-				},
-				GesamtNetto: com.Betrag{
-					Wert:     decimal.NewFromFloat(240),
-					Waehrung: waehrungscode.EUR,
-				},
-				GesamtSteuer: com.Betrag{
-					Wert:     decimal.NewFromFloat(18.36),
-					Waehrung: waehrungscode.EUR,
-				},
-				GesamtBrutto: com.Betrag{
-					Wert:     decimal.NewFromFloat(285.6),
-					Waehrung: waehrungscode.EUR,
-				},
-				Vorausgezahlt: &com.Betrag{
-					Wert:     decimal.NewFromFloat(85.6),
-					Waehrung: waehrungscode.EUR,
-				},
-				RabattBrutto: nil,
-				Zuzahlen: com.Betrag{
-					Wert:     decimal.NewFromFloat(200.),
-					Waehrung: waehrungscode.EUR,
-				},
-				Steuerbetraege: nil,
-				Rechnungspositionen: []com.Rechnungsposition{
-					{
-						Positionsnummer: 17,
-						LieferungVon:    time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC),
-						LieferungBis:    time.Date(2022, 8, 1, 0, 0, 0, 0, time.UTC),
-						Positionstext:   "foo",
-						Zeiteinheit:     zeiteinheit.Jahr,
-						Artikelnummer:   bdewartikelnummer.Abgabekwkg,
-						LokationsId:     "54321012345",
-						PositionsMenge: com.Menge{
-							Wert:    decimal.NewFromFloat(20),
-							Einheit: mengeneinheit.KWH,
-						},
-						Einzelpreis: com.Preis{
-							Wert:       decimal.NewFromFloat(12),
-							Einheit:    waehrungseinheit.EUR,
-							Bezugswert: mengeneinheit.KWH,
-							Status:     preisstatus.Endgueltig,
-						},
-						TeilsummeNetto: com.Betrag{
-							Wert:     decimal.NewFromFloat(240),
-							Waehrung: waehrungscode.EUR,
-						},
-						TeilsummeSteuer: com.Steuerbetrag{
-							Steuerkennzeichen: steuerkennzeichen.Ust19,
-							Basiswert:         decimal.NewFromFloat(240),
-							Steuerwert:        decimal.NewFromFloat(45.6),
-							Waehrung:          waehrungscode.EUR,
-						},
-						TeilrabattNetto: nil,
-					},
-				},
-			},
+			Rechnung:             CompleteValidRechnung,
 			Sparte:               sparte.Gas,
 			Absendercodenummer:   "9876543210987",
 			Empfaengercodenummer: "0123456789012",
--- bo/rechnung_test.go
+++ bo/rechnung_test.go
@@ -257,141 +257,143 @@ func (s *Suite) Test_Successful_Rechnung_Validation() {
 	validate := validator.New()
 	validate.RegisterStructValidation(RechnungStructLevelValidation, Rechnung{})
 	validRechnung := []interface{}{
-		Rechnung{
-			BusinessObject: BusinessObject{
-				BoTyp:             botyp.Rechnung,
-				VersionStruktur:   "1",
-				ExterneReferenzen: nil,
+		CompleteValidRechnung,
+	}
+	VerfiySuccessfulValidations(s, validate, validRechnung)
+}
+
+var CompleteValidRechnung = Rechnung{
+	BusinessObject: BusinessObject{
+		BoTyp:             botyp.Rechnung,
+		VersionStruktur:   "1",
+		ExterneReferenzen: nil,
+	},
+	Rechnungstitel:          "De",
+	Rechnungsstatus:         rechnungsstatus.Bezahlt,
+	Storno:                  true,
+	Rechnungsnummer:         "123",
+	Rechnungsdatum:          time.Date(2023, 8, 1, 0, 0, 0, 0, time.UTC),
+	Faelligkeitsdatum:       time.Date(2022, 8, 1, 0, 0, 0, 0, time.UTC),
+	Rechnungstyp:            rechnungstyp.Endkundenrechnung,
+	OriginalRechnungsnummer: "hallo",
+	Rechnungsperiode: com.Zeitraum{
+		Einheit:        zeiteinheit.Minute,
+		Dauer:          decimal.NullDecimal{Valid: true, Decimal: decimal.NewFromFloat(15)},
+		Startzeitpunkt: time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC),
+		Endzeitpunkt:   time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC).Add(time.Minute * 15),
+	},
+	Rechnungsersteller: Geschaeftspartner{
+		BusinessObject: BusinessObject{
+			BoTyp:             botyp.Geschaeftspartner,
+			VersionStruktur:   "1",
+			ExterneReferenzen: nil,
+		},
+		Anrede:               anrede.Divers,
+		Name1:                "Müller",
+		Name2:                "Lieschen",
+		Name3:                "",
+		Gewerbekennzeichnung: false,
+		HrNummer:             "handelsregister foo",
+		Amtsgericht:          "amtsgericht bar",
+		Kontaktweg:           0,
+		UmsatzsteuerId:       "umsatzsteuer foo",
+		GlaeubigerId:         "glauebiger bar",
+		EMailAdresse:         "email@lieschen-mueller.de",
+		Website:              "https://lieschen-mueller.de",
+		Geschaeftspartnerrollen: []geschaeftspartnerrolle.Geschaeftspartnerrolle{
+			geschaeftspartnerrolle.Kunde,
+			geschaeftspartnerrolle.Marktpartner,
+		},
+		Partneradresse: com.Adresse{
+			Postleitzahl: "82031",
+			Ort:          "Grünwald",
+			Strasse:      "Nördlicher Münchner Straße",
+			Hausnummer:   "27A",
+			Landescode:   landescode.DE,
+		},
+	},
+	Rechnungsempfaenger: Geschaeftspartner{
+		BusinessObject: BusinessObject{
+			BoTyp:             botyp.Geschaeftspartner,
+			VersionStruktur:   "1",
+			ExterneReferenzen: nil,
+		},
+		Anrede:               anrede.Divers,
+		Name1:                "Müller",
+		Name2:                "Lieschen",
+		Name3:                "",
+		Gewerbekennzeichnung: false,
+		HrNummer:             "handelsregister foo",
+		Amtsgericht:          "amtsgericht bar",
+		Kontaktweg:           0,
+		UmsatzsteuerId:       "umsatzsteuer foo",
+		GlaeubigerId:         "glauebiger bar",
+		EMailAdresse:         "email@lieschen-mueller.de",
+		Website:              "https://lieschen-mueller.de",
+		Geschaeftspartnerrollen: []geschaeftspartnerrolle.Geschaeftspartnerrolle{
+			geschaeftspartnerrolle.Kunde,
+			geschaeftspartnerrolle.Marktpartner,
+		},
+		Partneradresse: com.Adresse{
+			Postleitzahl: "82031",
+			Ort:          "Grünwald",
+			Strasse:      "Nördlicher Münchner Straße",
+			Hausnummer:   "27A",
+			Landescode:   landescode.DE,
+		},
+	},
+	GesamtNetto: com.Betrag{
+		Wert:     decimal.NewFromFloat(240),
+		Waehrung: waehrungscode.EUR,
+	},
+	GesamtSteuer: com.Betrag{
+		Wert:     decimal.NewFromFloat(18.36),
+		Waehrung: waehrungscode.EUR,
+	},
+	GesamtBrutto: com.Betrag{
+		Wert:     decimal.NewFromFloat(285.6),
+		Waehrung: waehrungscode.EUR,
+	},
+	Vorausgezahlt: &com.Betrag{
+		Wert:     decimal.NewFromFloat(85.6),
+		Waehrung: waehrungscode.EUR,
+	},
+	RabattBrutto: nil,
+	Zuzahlen: com.Betrag{
+		Wert:     decimal.NewFromFloat(200.),
+		Waehrung: waehrungscode.EUR,
+	},
+	Steuerbetraege: nil,
+	Rechnungspositionen: []com.Rechnungsposition{
+		{
+			Positionsnummer: 17,
+			LieferungVon:    time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC),
+			LieferungBis:    time.Date(2022, 8, 1, 0, 0, 0, 0, time.UTC),
+			Positionstext:   "foo",
+			Zeiteinheit:     zeiteinheit.Jahr,
+			Artikelnummer:   bdewartikelnummer.Abgabekwkg,
+			LokationsId:     "54321012345",
+			PositionsMenge: com.Menge{
+				Wert:    decimal.NewFromFloat(20),
+				Einheit: mengeneinheit.KWH,
 			},
-			Rechnungstitel:          "De",
-			Rechnungsstatus:         rechnungsstatus.Bezahlt,
-			Storno:                  true,
-			Rechnungsnummer:         "123",
-			Rechnungsdatum:          time.Date(2023, 8, 1, 0, 0, 0, 0, time.UTC),
-			Faelligkeitsdatum:       time.Date(2022, 8, 1, 0, 0, 0, 0, time.UTC),
-			Rechnungstyp:            rechnungstyp.Endkundenrechnung,
-			OriginalRechnungsnummer: "hallo",
-			Rechnungsperiode: com.Zeitraum{
-				Einheit:        zeiteinheit.Minute,
-				Dauer:          decimal.NullDecimal{Valid: true, Decimal: decimal.NewFromFloat(15)},
-				Startzeitpunkt: time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC),
-				Endzeitpunkt:   time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC).Add(time.Minute * 15),
+			Einzelpreis: com.Preis{
+				Wert:       decimal.NewFromFloat(12),
+				Einheit:    waehrungseinheit.EUR,
+				Bezugswert: mengeneinheit.KWH,
+				Status:     preisstatus.Endgueltig,
 			},
-			Rechnungsersteller: Geschaeftspartner{
-				BusinessObject: BusinessObject{
-					BoTyp:             botyp.Geschaeftspartner,
-					VersionStruktur:   "1",
-					ExterneReferenzen: nil,
-				},
-				Anrede:               anrede.Divers,
-				Name1:                "Müller",
-				Name2:                "Lieschen",
-				Name3:                "",
-				Gewerbekennzeichnung: false,
-				HrNummer:             "handelsregister foo",
-				Amtsgericht:          "amtsgericht bar",
-				Kontaktweg:           0,
-				UmsatzsteuerId:       "umsatzsteuer foo",
-				GlaeubigerId:         "glauebiger bar",
-				EMailAdresse:         "email@lieschen-mueller.de",
-				Website:              "https://lieschen-mueller.de",
-				Geschaeftspartnerrollen: []geschaeftspartnerrolle.Geschaeftspartnerrolle{
-					geschaeftspartnerrolle.Kunde,
-					geschaeftspartnerrolle.Marktpartner,
-				},
-				Partneradresse: com.Adresse{
-					Postleitzahl: "82031",
-					Ort:          "Grünwald",
-					Strasse:      "Nördlicher Münchner Straße",
-					Hausnummer:   "27A",
-					Landescode:   landescode.DE,
-				},
-			},
-			Rechnungsempfaenger: Geschaeftspartner{
-				BusinessObject: BusinessObject{
-					BoTyp:             botyp.Geschaeftspartner,
-					VersionStruktur:   "1",
-					ExterneReferenzen: nil,
-				},
-				Anrede:               anrede.Divers,
-				Name1:                "Müller",
-				Name2:                "Lieschen",
-				Name3:                "",
-				Gewerbekennzeichnung: false,
-				HrNummer:             "handelsregister foo",
-				Amtsgericht:          "amtsgericht bar",
-				Kontaktweg:           0,
-				UmsatzsteuerId:       "umsatzsteuer foo",
-				GlaeubigerId:         "glauebiger bar",
-				EMailAdresse:         "email@lieschen-mueller.de",
-				Website:              "https://lieschen-mueller.de",
-				Geschaeftspartnerrollen: []geschaeftspartnerrolle.Geschaeftspartnerrolle{
-					geschaeftspartnerrolle.Kunde,
-					geschaeftspartnerrolle.Marktpartner,
-				},
-				Partneradresse: com.Adresse{
-					Postleitzahl: "82031",
-					Ort:          "Grünwald",
-					Strasse:      "Nördlicher Münchner Straße",
-					Hausnummer:   "27A",
-					Landescode:   landescode.DE,
-				},
-			},
-			GesamtNetto: com.Betrag{
+			TeilsummeNetto: com.Betrag{
 				Wert:     decimal.NewFromFloat(240),
 				Waehrung: waehrungscode.EUR,
 			},
-			GesamtSteuer: com.Betrag{
-				Wert:     decimal.NewFromFloat(18.36),
-				Waehrung: waehrungscode.EUR,
+			TeilsummeSteuer: com.Steuerbetrag{
+				Steuerkennzeichen: steuerkennzeichen.Ust19,
+				Basiswert:         decimal.NewFromFloat(240),
+				Steuerwert:        decimal.NewFromFloat(45.6),
+				Waehrung:          waehrungscode.EUR,
 			},
-			GesamtBrutto: com.Betrag{
-				Wert:     decimal.NewFromFloat(285.6),
-				Waehrung: waehrungscode.EUR,
-			},
-			Vorausgezahlt: &com.Betrag{
-				Wert:     decimal.NewFromFloat(85.6),
-				Waehrung: waehrungscode.EUR,
-			},
-			RabattBrutto: nil,
-			Zuzahlen: com.Betrag{
-				Wert:     decimal.NewFromFloat(200.),
-				Waehrung: waehrungscode.EUR,
-			},
-			Steuerbetraege: nil,
-			Rechnungspositionen: []com.Rechnungsposition{
-				{
-					Positionsnummer: 17,
-					LieferungVon:    time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC),
-					LieferungBis:    time.Date(2022, 8, 1, 0, 0, 0, 0, time.UTC),
-					Positionstext:   "foo",
-					Zeiteinheit:     zeiteinheit.Jahr,
-					Artikelnummer:   bdewartikelnummer.Abgabekwkg,
-					LokationsId:     "54321012345",
-					PositionsMenge: com.Menge{
-						Wert:    decimal.NewFromFloat(20),
-						Einheit: mengeneinheit.KWH,
-					},
-					Einzelpreis: com.Preis{
-						Wert:       decimal.NewFromFloat(12),
-						Einheit:    waehrungseinheit.EUR,
-						Bezugswert: mengeneinheit.KWH,
-						Status:     preisstatus.Endgueltig,
-					},
-					TeilsummeNetto: com.Betrag{
-						Wert:     decimal.NewFromFloat(240),
-						Waehrung: waehrungscode.EUR,
-					},
-					TeilsummeSteuer: com.Steuerbetrag{
-						Steuerkennzeichen: steuerkennzeichen.Ust19,
-						Basiswert:         decimal.NewFromFloat(240),
-						Steuerwert:        decimal.NewFromFloat(45.6),
-						Waehrung:          waehrungscode.EUR,
-					},
-					TeilrabattNetto: nil,
-				},
-			},
+			TeilrabattNetto: nil,
 		},
-	}
-	VerfiySuccessfulValidations(s, validate, validRechnung)
+	},
 }
