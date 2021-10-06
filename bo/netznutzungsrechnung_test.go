af1c0e452aeab167497aa6e87b09ab420cd1b026
konstantin <konstantin.klein@hochfrequenz.de>
2021-10-06 08:52:58 +0200

    Fix another code duplication

--- bo/netznutzungsrechnung_test.go
+++ bo/netznutzungsrechnung_test.go
@@ -5,126 +5,15 @@ import (
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
 	"github.com/go-playground/validator/v10"
-	"github.com/hochfrequenz/go-bo4e/com"
-	"github.com/hochfrequenz/go-bo4e/enum/anrede"
-	"github.com/hochfrequenz/go-bo4e/enum/botyp"
-	"github.com/hochfrequenz/go-bo4e/enum/geschaeftspartnerrolle"
-	"github.com/hochfrequenz/go-bo4e/enum/landescode"
 	"github.com/hochfrequenz/go-bo4e/enum/nnrechnungsart"
 	"github.com/hochfrequenz/go-bo4e/enum/nnrechnungstyp"
-	"github.com/hochfrequenz/go-bo4e/enum/rechnungsstatus"
-	"github.com/hochfrequenz/go-bo4e/enum/rechnungstyp"
 	"github.com/hochfrequenz/go-bo4e/enum/sparte"
-	"github.com/hochfrequenz/go-bo4e/enum/waehrungscode"
-	"github.com/hochfrequenz/go-bo4e/enum/zeiteinheit"
-	"github.com/shopspring/decimal"
-	"time"
 )
 
 // Test_Netznutzungsrechnung_Deserialization deserializes an Netznutzungsrechnung json
 func (s *Suite) Test_Netznutzungsrechnung_Deserialization() {
 	var nnrechnung = Netznutzungsrechnung{
-		Rechnung: Rechnung{
-			BusinessObject: BusinessObject{
-				BoTyp:             botyp.Rechnung,
-				VersionStruktur:   "1",
-				ExterneReferenzen: nil,
-			},
-			Rechnungstitel:          "De",
-			Rechnungsstatus:         rechnungsstatus.Bezahlt,
-			Storno:                  false,
-			Rechnungsnummer:         "123",
-			Rechnungsdatum:          time.Date(2023, 8, 1, 0, 0, 0, 0, time.UTC),
-			Faelligkeitsdatum:       time.Date(2022, 8, 1, 0, 0, 0, 0, time.UTC),
-			Rechnungstyp:            rechnungstyp.Endkundenrechnung,
-			OriginalRechnungsnummer: "hallo",
-			Rechnungsperiode: com.Zeitraum{
-				Einheit:        zeiteinheit.Minute,
-				Dauer:          decimal.NullDecimal{},
-				Startzeitpunkt: time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC),
-				Endzeitpunkt:   time.Date(2021, 8, 1, 0, 0, 0, 0, time.UTC).Add(time.Minute * 15),
-			},
-			Rechnungsersteller: Geschaeftspartner{
-				BusinessObject: BusinessObject{
-					BoTyp:             botyp.Geschaeftspartner,
-					VersionStruktur:   "1",
-					ExterneReferenzen: nil,
-				},
-				Anrede:               anrede.Divers,
-				Name1:                "Müller",
-				Name2:                "Lieschen",
-				Name3:                "",
-				Gewerbekennzeichnung: false,
-				HrNummer:             "handelsregister foo",
-				Amtsgericht:          "amtsgericht bar",
-				Kontaktweg:           0,
-				UmsatzsteuerId:       "umsatzsteuer foo",
-				GlaeubigerId:         "glauebiger bar",
-				EMailAdresse:         "email@lieschen-mueller.de",
-				Website:              "https://lieschen-mueller.de",
-				Geschaeftspartnerrollen: []geschaeftspartnerrolle.Geschaeftspartnerrolle{
-					geschaeftspartnerrolle.Kunde,
-					geschaeftspartnerrolle.Marktpartner,
-				},
-				Partneradresse: com.Adresse{
-					Postleitzahl: "82031",
-					Ort:          "Grünwald",
-					Strasse:      "Nördlicher Münchner Straße",
-					Hausnummer:   "27A",
-					Landescode:   landescode.DE,
-				},
-			},
-			Rechnungsempfaenger: Geschaeftspartner{
-				BusinessObject: BusinessObject{
-					BoTyp:             botyp.Geschaeftspartner,
-					VersionStruktur:   "1",
-					ExterneReferenzen: nil,
-				},
-				Anrede:               anrede.Divers,
-				Name1:                "Müller",
-				Name2:                "Lieschen",
-				Name3:                "",
-				Gewerbekennzeichnung: false,
-				HrNummer:             "handelsregister foo",
-				Amtsgericht:          "amtsgericht bar",
-				Kontaktweg:           0,
-				UmsatzsteuerId:       "umsatzsteuer foo",
-				GlaeubigerId:         "glauebiger bar",
-				EMailAdresse:         "email@lieschen-mueller.de",
-				Website:              "https://lieschen-mueller.de",
-				Geschaeftspartnerrollen: []geschaeftspartnerrolle.Geschaeftspartnerrolle{
-					geschaeftspartnerrolle.Kunde,
-					geschaeftspartnerrolle.Marktpartner,
-				},
-				Partneradresse: com.Adresse{
-					Postleitzahl: "82031",
-					Ort:          "Grünwald",
-					Strasse:      "Nördlicher Münchner Straße",
-					Hausnummer:   "27A",
-					Landescode:   landescode.DE,
-				},
-			},
-			GesamtNetto: com.Betrag{
-				Wert:     decimal.NewFromFloat(18.36),
-				Waehrung: waehrungscode.EUR,
-			},
-			GesamtSteuer: com.Betrag{
-				Wert:     decimal.NewFromFloat(18.36),
-				Waehrung: waehrungscode.EUR,
-			},
-			GesamtBrutto: com.Betrag{
-				Wert:     decimal.NewFromFloat(18.36),
-				Waehrung: waehrungscode.EUR,
-			},
-			Vorausgezahlt: nil,
-			RabattBrutto:  nil,
-			Zuzahlen: com.Betrag{
-				Wert:     decimal.NewFromFloat(18.36),
-				Waehrung: waehrungscode.EUR,
-			},
-			Steuerbetraege:      nil,
-			Rechnungspositionen: nil,
-		},
+		Rechnung:             CompleteValidRechnung,
 		Sparte:               sparte.Gas,
 		Absendercodenummer:   "9876543210987",
 		Empfaengercodenummer: "0123456789012",
