b408401d767762caa1fbc668197553e3f2b6b191
konstantin <konstantin.klein@hochfrequenz.de>
2021-10-24 18:36:26 +0200

    implement "setNachrichtum" method

--- market_communication/boney_comb_transaktionsdaten.go
+++ market_communication/boney_comb_transaktionsdaten.go
@@ -19,25 +19,33 @@ func (boneyComb *BOneyComb) GetTransactionData(key string) *string {
 
 // the following 4 properties are present in any boneycomb: Dokumentennummer, Nachrichtendatum, Absender, Empfaenger,
 
-// dokumentnummerkey is the key under which the Dokumentennummer is stored in the BOneyComb.Transaktionsdaten
-const dokumentnummerkey = "Dokumentennummer"
+// initializeTransaktionsdaten sets BOneyComb.Transaktionsdaten to an empty map iff it is nil
+func (boneyComb *BOneyComb) initializeTransaktionsdaten() {
+	if boneyComb.Transaktionsdaten == nil {
+		boneyComb.Transaktionsdaten = map[string]string{}
+	}
+}
+
+// dokumentnummerKey is the key under which the Dokumentennummer is stored in the BOneyComb.Transaktionsdaten
+const dokumentnummerKey = "Dokumentennummer"
+
+// nachrichtendatumKey is the key under which the Nachrichtendatum is stored in the BOneyComb.Transaktionsdaten
+const nachrichtendatumKey = "Nachrichtendatum"
 
 // GetDokumentennummer returns the Dokumentennummer from BOneyComb.Transaktionsdaten if it's present and nil otherwise
 func (boneyComb *BOneyComb) GetDokumentennummer() *string {
-	return boneyComb.GetTransactionData(dokumentnummerkey)
+	return boneyComb.GetTransactionData(dokumentnummerKey)
 }
 
 // SetDokumentennummer sets the Dokumentennummer in the BOneyComb.Transaktionsdaten
 func (boneyComb *BOneyComb) SetDokumentennummer(dokumentnummer string) {
-	if boneyComb.Transaktionsdaten == nil {
-		boneyComb.Transaktionsdaten = map[string]string{}
-	}
-	boneyComb.Transaktionsdaten[dokumentnummerkey] = dokumentnummer
+	boneyComb.initializeTransaktionsdaten()
+	boneyComb.Transaktionsdaten[dokumentnummerKey] = dokumentnummer
 }
 
 // GetNachrichtendatum checks if the message date is present in BOneyComb.Transaktionsdaten, returns its value if it is present and nil otherwise. Returns an error iff the parsing fails
 func (boneyComb *BOneyComb) GetNachrichtendatum() (*time.Time, error) {
-	stringlyTypedNachrichtenDatum := boneyComb.GetTransactionData("Nachrichtendatum")
+	stringlyTypedNachrichtenDatum := boneyComb.GetTransactionData(nachrichtendatumKey)
 	if stringlyTypedNachrichtenDatum == nil || *stringlyTypedNachrichtenDatum == "" {
 		return nil, nil
 	}
@@ -46,4 +54,10 @@ func (boneyComb *BOneyComb) GetNachrichtendatum() (*time.Time, error) {
 		return nil, err
 	}
 	return &nachrichtendatum, nil
+}
+
+// SetNachrichtendatum sets the nachrichtendatum in BOneyComb.Transaktionsdaten
+func (boneyComb *BOneyComb) SetNachrichtendatum(nachrichtendatum time.Time) {
+	boneyComb.initializeTransaktionsdaten()
+	boneyComb.Transaktionsdaten[nachrichtendatumKey] = nachrichtendatum.Format(time.RFC3339)
 }
