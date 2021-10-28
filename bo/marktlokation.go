83af97002423d6229b4788515b82f2888e842263
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2021-10-28 14:11:53 +0200

    ✨Add BO Marktlokation

---
+++ bo/marktlokation.go
@@ -0,0 +1,36 @@
+package bo
+
+import (
+	"github.com/hochfrequenz/go-bo4e/com"
+	"github.com/hochfrequenz/go-bo4e/enum/bilanzierungsmethode"
+	"github.com/hochfrequenz/go-bo4e/enum/energierichtung"
+	"github.com/hochfrequenz/go-bo4e/enum/gasqualitaet"
+	"github.com/hochfrequenz/go-bo4e/enum/gebiettyp"
+	"github.com/hochfrequenz/go-bo4e/enum/netzebene"
+	"github.com/hochfrequenz/go-bo4e/enum/sparte"
+	"github.com/hochfrequenz/go-bo4e/enum/verbrauchsart"
+)
+
+// Marktlokation contains information about a market location aka "MaLo"
+type Marktlokation struct {
+	Geschaeftsobjekt
+	MarktlokationsId     string                                    `json:"marktlokationsId" example:"" validate:"required,numeric,len=11"` // ID of the market location
+	Sparte               sparte.Sparte                             `json:"sparte" validate:"required"`                                     // Division
+	Energierichtung      energierichtung.Energierichtung           `json:"energierichtung" validate:"required"`                            // Identification of whether energy is supplied out of or fed into the grid.
+	Bilanzierungsmethode bilanzierungsmethode.Bilanzierungsmethode `json:"bilanzierungsmethode" validate:"required"`                       // Accounting method
+	Verbrauchsart        verbrauchsart.Verbrauchsart               `json:"verbrauchsart,omitempty"`                                        // Consumption type
+	Unterbrechbar        bool                                      `json:"unterbrechbar,omitempty"`                                        // Identification whether the supply is interruptible
+	Netzebene            netzebene.Netzebene                       `json:"netzebene" validate:"required"`                                  // Netzebene, in der der Bezug der Energie erfolgt. Bei Strom Spannungsebene der Lieferung, bei Gas Druckstufe.
+	Netzbetreibercodenr  string                                    `json:"netzbetreibercodenr,omitempty" validate:"omitempty,numeric"`     // Code number of the "Netzbetreiber"
+	Gebiettyp            gebiettyp.Gebiettyp                       `json:"gebiettyp,omitempty"`                                            // Type of the "Netzgebiet"
+	Netzgebietnr         string                                    `json:"netzgebietnr,omitempty"`                                         // Number of the "Netzgebiet" in the enet database
+	Bilanzierungsgebiet  string                                    `json:"bilanzierungsgebiet,omitempty"`                                  // Bilanzierungsgebiet, dem das Netzgebiet zugeordnet ist - im Falle eines Strom Netzes.
+	Grundversorgercodenr string                                    `json:"grundversorgercodenr,omitempty"`                                 // Code number of the "Grundversorger" responsible for this market location
+	Gasqualitaet         gasqualitaet.Gasqualitaet                 `json:"gasqualitaet,omitempty"`                                         // Gas Quality
+	Endkunde             Geschaeftspartner                         `json:"endkunde,omitempty"`                                             // Link to the Geschaeftspartner who owns this market location
+	// only one of the following three optional address attributes can be set
+	Lokationsadresse          *com.Adresse                 `json:"lokationsadresse" validate:"required_without_all=Geoadresse KatasterInformation"` // Address at which the energy supply or feed-in takes place
+	Geoadresse                *com.Geokoordinaten          `json:"geoadresse" validate:"required_without_all=Lokationsadresse KatasterInformation"` // Gps coordinates
+	Katasterinformation       *com.Katasteradresse         `json:"katasterinformation" validate:"required_without_all=Lokationsadresse Geoadresse"` // Cadastre address
+	ZugehoerigeMesslokationen []com.Messlokationszuordnung `json:"zugehoerigemesslokationen,omitempty"`                                             // List of MeLos belonging to this market location
+}
