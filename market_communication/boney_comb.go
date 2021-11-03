55f37b86fbdd4f73a8192952edd5914832634b6f
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-03 11:42:43 +0100

    BOneyComb Struct Level Validation to check for pruefi

--- market_communication/boney_comb.go
+++ market_communication/boney_comb.go
@@ -1,7 +1,9 @@
 package market_communication
 
 import (
+	"github.com/go-playground/validator/v10"
 	"github.com/hochfrequenz/go-bo4e/bo"
+	"regexp"
 )
 
 // BOneyComb is a structure that is used when dealing with business objects that are embedded into market communication messages.
@@ -9,4 +11,17 @@ import (
 type BOneyComb struct {
 	Stammdaten        bo.BusinessObjectSlice `json:"stammdaten" validate:"required"`        // Stammdaten is an array of business objects
 	Transaktionsdaten map[string]string      `json:"transaktionsdaten" validate:"required"` // Transaktionsdaten are data relevant only in the context of this market communication message
+}
+
+var pruefiPattern = regexp.MustCompile("^[1-9]\\d{4}$")
+
+// PruefidentifikatorInTransaktionsdatenValidation returns true iff a valid Pruefidentifikator (see GetPruefidentifikator) is present
+func PruefidentifikatorInTransaktionsdatenValidation(sl validator.StructLevel) {
+	boneyComb := sl.Current().Interface().(BOneyComb)
+	pruefi := boneyComb.GetPruefidentifikator()
+	if pruefi == nil {
+		sl.ReportError(boneyComb.Stammdaten, "Pruefidentifikator", "Pruefidentifikator", "Pruefi must not be null", "")
+	} else if !pruefiPattern.MatchString(*pruefi) {
+		sl.ReportError(boneyComb.Stammdaten, "Pruefidentifikator", "Pruefidentifikator", "Pruefi must be set and 5 digits long", "")
+	}
 }
