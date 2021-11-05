8ed8c2c2fb7526eada7cc9bd3b7ca9eeacb66d00
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2021-11-05 17:01:22 +0100

    ✅Add tests for custom validations

--- bo/marktlokation_test.go
+++ bo/marktlokation_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
+	"github.com/go-playground/validator/v10"
 	"github.com/hochfrequenz/go-bo4e/bo"
 	"github.com/hochfrequenz/go-bo4e/com"
 	"github.com/hochfrequenz/go-bo4e/enum/arithmetischeoperation"
@@ -11,6 +12,7 @@ import (
 	"github.com/hochfrequenz/go-bo4e/enum/botyp"
 	"github.com/hochfrequenz/go-bo4e/enum/energierichtung"
 	"github.com/hochfrequenz/go-bo4e/enum/gebiettyp"
+	"github.com/hochfrequenz/go-bo4e/enum/geschaeftspartnerrolle"
 	"github.com/hochfrequenz/go-bo4e/enum/landescode"
 	"github.com/hochfrequenz/go-bo4e/enum/netzebene"
 	"github.com/hochfrequenz/go-bo4e/enum/sparte"
@@ -47,6 +49,16 @@ func (s *Suite) Test_Marktlokation_Deserialization() {
 			},
 			Name1:                "Kruemel",
 			Gewerbekennzeichnung: false,
+			Partneradresse: com.Adresse{
+				Postleitzahl: "82031",
+				Ort:          "Grünwald",
+				Strasse:      "Nördlicher Münchner Straße",
+				Hausnummer:   "27A",
+				Landescode:   landescode.DE,
+			},
+			Geschaeftspartnerrollen: []geschaeftspartnerrolle.Geschaeftspartnerrolle{
+				geschaeftspartnerrolle.DIENSTLEISTER,
+			},
 		},
 		Lokationsadresse: &com.Adresse{
 			Postleitzahl: "82031",
@@ -77,6 +89,83 @@ func (s *Suite) Test_Marktlokation_Deserialization() {
 	err = json.Unmarshal(serializedMalo, &deserializedMalo)
 	then.AssertThat(s.T(), err, is.Nil())
 	then.AssertThat(s.T(), deserializedMalo, is.EqualTo(malo))
+}
+
+// TestFailedMarktlokationValidation verifies that the validators of Marktlokation work
+func (s *Suite) Test_Failed_MarktlokationValidation() {
+	validate := validator.New()
+	registerError := validate.RegisterValidation("maloid", bo.MaloIdFieldLevelValidation)
+	then.AssertThat(s.T(), registerError, is.Nil())
+	validate.RegisterStructValidation(bo.XorStructLevelValidation, bo.Marktlokation{})
+
+	invalidMarktlokationMap := map[string][]interface{}{
+		"required": {
+			bo.Marktlokation{
+				Geschaeftsobjekt: bo.Geschaeftsobjekt{
+					BoTyp:             0,
+					VersionStruktur:   "",
+					ExterneReferenzen: nil,
+				},
+				MarktlokationsId:     "",
+				Sparte:               0,
+				Energierichtung:      0,
+				Bilanzierungsmethode: 0,
+				Netzebene:            0,
+			},
+		},
+		"onlyOneAddressType": {
+			bo.Marktlokation{
+				Lokationsadresse: &com.Adresse{
+					Postleitzahl: "82031",
+					Ort:          "Grünwald",
+					Strasse:      "Nördliche Münchner Straße",
+					Hausnummer:   "27A",
+					Landescode:   landescode.DE,
+				},
+				Katasterinformation: &com.Katasteradresse{
+					GemarkungFlur: "Foo",
+					Flurstueck:    "Bar",
+				},
+			},
+		},
+		"maloid": {
+			bo.Marktlokation{
+				MarktlokationsId: "12345678918",
+			},
+		},
+	}
+	VerfiyFailedValidations(s, validate, invalidMarktlokationMap)
+}
+
+//  Test_Successful_Marktlokation_Validation verifies that a valid Marktlokation is validated without errors
+func (s *Suite) Test_Successful_Marktlokation_Validation() {
+	validate := validator.New()
+	registerError := validate.RegisterValidation("maloid", bo.MaloIdFieldLevelValidation)
+	then.AssertThat(s.T(), registerError, is.Nil())
+	validate.RegisterStructValidation(bo.XorStructLevelValidation, bo.Marktlokation{})
+	validMalos := []bo.BusinessObject{
+		// Minimal attributes
+		bo.Marktlokation{
+			Geschaeftsobjekt: bo.Geschaeftsobjekt{
+				BoTyp:             botyp.MARKTLOKATION,
+				VersionStruktur:   "1",
+				ExterneReferenzen: nil,
+			},
+			MarktlokationsId:     "12345678913",
+			Sparte:               sparte.STROM,
+			Energierichtung:      energierichtung.EINSP,
+			Bilanzierungsmethode: bilanzierungsmethode.SLP,
+			Netzebene:            netzebene.NSP,
+			Lokationsadresse: &com.Adresse{
+				Postleitzahl: "82031",
+				Ort:          "Grünwald",
+				Strasse:      "Nördliche Münchner Straße",
+				Hausnummer:   "27A",
+				Landescode:   landescode.DE,
+			},
+		},
+	}
+	VerfiySuccessfulValidations(s, validate, validMalos)
 }
 
 func (s *Suite) Test_Empty_Marktlokation_Is_Creatable_Using_BoTyp() {
