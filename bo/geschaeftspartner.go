7e04a1e800d03400387e6121d02e73ce8837f962
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2021-11-05 17:16:50 +0100

    gofmt

--- bo/geschaeftspartner.go
+++ bo/geschaeftspartner.go
@@ -10,7 +10,7 @@ import (
 // Geschaeftspartner models business partners, both companies and private persons.
 type Geschaeftspartner struct {
 	Geschaeftsobjekt
-	Anrede                  anrede.Anrede                                   `json:"anrede,omitempty"`                                                                         // Die Anrede für den GePa, Z.B. HERR
+	Anrede                  anrede.Anrede                                   `json:"anrede,omitempty"`                                                               // Die Anrede für den GePa, Z.B. HERR
 	Name1                   string                                          `json:"name1" validate:"required" example:"Yellow Strom GmbH,Hagen"`                    // Erster Teil des Namens. Hier kann der Firmenname oder bei Privatpersonen beispielsweise der Nachname dargestellt werden.
 	Name2                   string                                          `json:"name2,omitempty" example:"'Bereich Süd','Nina'"`                                 // Zweiter Teil des Namens. Hier kann der eine Erweiterung zum Firmennamen oder bei Privatpersonen beispielsweise der Vorname dargestellt werden.
 	Name3                   string                                          `json:"name3,omitempty" example:"Afrika,Sängerin"`                                      // Hier können weitere Ergänzungen zum Firmennamen oder bei Privatpersonen Zusätze zum Namen dargestellt werden
--- enum/bilanzierungsmethode/bilanzierungsmethode.go
+++ enum/bilanzierungsmethode/bilanzierungsmethode.go
@@ -8,8 +8,8 @@ type Bilanzierungsmethode int
 const (
 	// RLM is a Registrierende Leistungsmessung
 	RLM           Bilanzierungsmethode = iota + 1
-	SLP                           // SLP is a Standard Lastprofil
-	TLP_GEMEINSAM                 // TLP_GEMEINSAM is a TLP gemeinsame Messung
-	TLP_GETRENNT                  // TLP_GETRENNT is a TLP getrennte Messung
-	PAUSCHAL                      // PAUSCHAL is a Pauschale Betrachtung (Band)
+	SLP                                // SLP is a Standard Lastprofil
+	TLP_GEMEINSAM                      // TLP_GEMEINSAM is a TLP gemeinsame Messung
+	TLP_GETRENNT                       // TLP_GETRENNT is a TLP getrennte Messung
+	PAUSCHAL                           // PAUSCHAL is a Pauschale Betrachtung (Band)
 )
