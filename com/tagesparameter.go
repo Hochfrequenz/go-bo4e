ff448df77b1b48f42c77c8320f6f2ba0f751e929
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-15 15:08:19 +0100

    add COM Tagesparameter

---
+++ com/tagesparameter.go
@@ -0,0 +1,8 @@
+package com
+
+// Tagesparameter speichert Informationen zu einer tagesparameter-abhängigen Messstelle. z.B. den Namen einer Klimazone oder die ID der Wetterstation für die Temperaturmessstelle
+type Tagesparameter struct {
+	Klimazone            string `json:"klimazone" validate:"required,alphanum" example:"7624q"`            // Klimazone is the qualifier of the climate zone
+	Temperaturmessstelle string `json:"temperaturmessstelle" validate:"required,alphanum" example:"1234x"` // Klimazone is the qualifier of the temperature measurement station
+	Dienstanbieter       string `json:"dienstanbieter" validate:"required_with=temperaturmessstelle"`          // Dienstanbieter is the service provider for temperature measurements
+}
