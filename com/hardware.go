25174bc6f8ea672a5ce3af300731ec95813b0ca0
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-15 17:12:26 +0100

    🎨 Harmonize Docstrings (#45)

--- com/hardware.go
+++ com/hardware.go
@@ -5,5 +5,5 @@ import "github.com/hochfrequenz/go-bo4e/enum/geraetetyp"
 // A Hardware is a billable device
 type Hardware struct {
 	GeraeteTyp  geraetetyp.Geraetetyp `json:"geraeteTyp,omitempty" validate:"required"`     // GeraeteTyp type of the hardware
-	Bezeichnung string                `json:"bezeichnung" validate:"alphaunicode,required"` // description
+	Bezeichnung string                `json:"bezeichnung" validate:"alphaunicode,required"` // Bezeichnung is a description
 }
--- com/preis.go
+++ com/preis.go
@@ -9,8 +9,8 @@ import (
 
 // Preis is a price
 type Preis struct {
-	Wert       decimal.Decimal                   `json:"wert" validate:"required"`                 // Gibt die nominale Höhe des Preises an
-	Einheit    waehrungseinheit.Waehrungseinheit `json:"einheit,omitempty" validate:"required"`    // Währungseinheit für den Preis
-	Bezugswert mengeneinheit.Mengeneinheit       `json:"bezugswert,omitempty" validate:"required"` // Angabe, für welche Bezugsgröße der Preis gilt. Z.B. kWh
-	Status     preisstatus.Preisstatus           `json:"status,omitempty"`                         // Gibt den Status des veröffentlichten Preises an, Details siehe ENUM Preisstatus
+	Wert       decimal.Decimal                   `json:"wert" validate:"required"`                 // Wert gibt die nominale Höhe des Preises an
+	Einheit    waehrungseinheit.Waehrungseinheit `json:"einheit,omitempty" validate:"required"`    // Einheit ist die Währungseinheit für den Preis
+	Bezugswert mengeneinheit.Mengeneinheit       `json:"bezugswert,omitempty" validate:"required"` // Bezugswert ist eine Angabe, für welche Bezugsgröße der Preis gilt. Z.B. kWh
+	Status     preisstatus.Preisstatus           `json:"status,omitempty"`                         // Status gibt den Status des veröffentlichten Preises an
 }
