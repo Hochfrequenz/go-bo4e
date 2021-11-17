96d3eb623c91d7f5846a672e3cdb5537c1adad8c
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-17 14:06:50 +0100

    rm validation tag on nullable boolean

--- com/lastprofil.go
+++ com/lastprofil.go
@@ -7,6 +7,6 @@ import "github.com/hochfrequenz/go-bo4e/enum/profilverfahren"
 type Lastprofil struct {
 	Bezeichnung    string                          `json:"bezeichnung" validate:"omitempty,alphanum" example:"H0"` // Bezeichnung des Profils, durch DVGW bzw. den Netzbetreiber vergeben
 	Verfahren      profilverfahren.Profilverfahren `json:"verfahren,omitempty"`                                    // Verfahren des Profils (analytisch oder synthetisch)
-	Einspeisung    *bool                           `json:"einspeisung,omitempty" validate:"omitempty"`             // Einspeisung ist true, falls es sich um Einspeisung handelt
+	Einspeisung    *bool                           `json:"einspeisung,omitempty"`                                  // Einspeisung ist true, falls es sich um Einspeisung handelt
 	Tagesparameter *Tagesparameter                 `json:"tagesparameter,omitempty"`                               // Tagesparameter contains the Klimazone or Temperaturmessstelle
 }
