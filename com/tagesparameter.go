0bddb8b47a1d2935cf787f295de729cc1a578cb7
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-17 12:38:41 +0100

    thanks for the test

--- com/tagesparameter.go
+++ com/tagesparameter.go
@@ -4,5 +4,5 @@ package com
 type Tagesparameter struct {
 	Klimazone            string `json:"klimazone" validate:"required,alphanum" example:"7624q"`            // Klimazone is the qualifier of the climate zone
 	Temperaturmessstelle string `json:"temperaturmessstelle" validate:"required,alphanum" example:"1234x"` // Klimazone is the qualifier of the temperature measurement station
-	Dienstanbieter       string `json:"dienstanbieter" validate:"required_with=temperaturmessstelle"`      // Dienstanbieter is the service provider for temperature measurements
+	Dienstanbieter       string `json:"dienstanbieter" validate:"required_with=Temperaturmessstelle"`      // Dienstanbieter is the service provider for temperature measurements
 }
