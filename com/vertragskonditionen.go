c6da679f85f92a3c35c8d2d3c43c21b9b606ebd6
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-19 12:44:32 +0100

    🎨Make vertragkonditionen->Zeitraums Nullable (#55)
    * Make vertragkonditionen->Zeitraums nullable
    
    * reduce code duplication in tests

--- com/vertragskonditionen.go
+++ com/vertragskonditionen.go
@@ -9,12 +9,12 @@ import (
 
 // Vertragskonditionen ist eine Komponente zur Abbildung von Vertragskonditionen. Die Komponente wird sowohl im Vertrag als auch im Tarif verwendet.
 type Vertragskonditionen struct {
-	Beschreibung          string   `json:"beschreibung,omitempty"`          // Beschreibung ist ein Freitext zur Beschreibung der Konditionen, z.B. "Standardkonditionen Gas"
-	AnzahlAbschlaege      int      `json:"anzahlAbschlaege,omitempty"`      // AnzahlAbschlaege enthält die Anzahl der vereinbarten Abschläge pro Jahr, z.B. 12
-	Vertragslaufzeit      Zeitraum `json:"vertragslaufzeit,omitempty"`      // Vertragslaufzeit enthält den Zeitraum, über den dieser Vertrag läuft
-	Kuendigungsfrist      Zeitraum `json:"kuendigungsfrist,omitempty"`      // Kuendigungsfrist ist die Frist, innerhalb derer der Vertrag gekündigt werden kann
-	Vertragsverlaengerung Zeitraum `json:"vertragsverlaengerung,omitempty"` // Vertragsverlaengerung beschreibt, dass, falls der Vertrag nicht gekündigt wird, er sich automatisch um die angegebene Zeit verlängert
-	Abschlagszyklus       Zeitraum `json:"abschlagszyklus,omitempty"`       // Abschlagszyklus sind die Zyklen, in denen Abschläge erstellt werden. Alternativ kann auch die Anzahl in den Konditionen angeben werden.
+	Beschreibung          string    `json:"beschreibung,omitempty"`                               // Beschreibung ist ein Freitext zur Beschreibung der Konditionen, z.B. "Standardkonditionen Gas"
+	AnzahlAbschlaege      int       `json:"anzahlAbschlaege,omitempty"`                           // AnzahlAbschlaege enthält die Anzahl der vereinbarten Abschläge pro Jahr, z.B. 12
+	Vertragslaufzeit      *Zeitraum `json:"vertragslaufzeit,omitempty" validate:"omitempty"`      // Vertragslaufzeit enthält den Zeitraum, über den dieser Vertrag läuft
+	Kuendigungsfrist      *Zeitraum `json:"kuendigungsfrist,omitempty" validate:"omitempty"`      // Kuendigungsfrist ist die Frist, innerhalb derer der Vertrag gekündigt werden kann
+	Vertragsverlaengerung *Zeitraum `json:"vertragsverlaengerung,omitempty" validate:"omitempty"` // Vertragsverlaengerung beschreibt, dass, falls der Vertrag nicht gekündigt wird, er sich automatisch um die angegebene Zeit verlängert
+	Abschlagszyklus       *Zeitraum `json:"abschlagszyklus,omitempty" validate:"omitempty"`       // Abschlagszyklus sind die Zyklen, in denen Abschläge erstellt werden. Alternativ kann auch die Anzahl in den Konditionen angeben werden.
 
 	// the fields below are not bo4e standard (yet)
 
