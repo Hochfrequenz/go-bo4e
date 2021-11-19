cc713fa333cbcb0a45552cac50cebc8c486794fa
konstantin <konstantin.klein@hochfrequenz.de>
2021-11-19 15:09:14 +0100

    Enhance Zaehler and Zaehlwerke with non BO4E Standard Fields (#59)

---
+++ enum/schwachlastfaehigkeit/schwachlastfaehigkeit.go
@@ -0,0 +1,14 @@
+package schwachlastfaehigkeit
+
+// Schwachlastfaehigkeit ist de facto ein boolean der die Schwachlastfaehigkeit beschreibt
+// Note that this is not official BO4E standard (yet).
+//go:generate stringer --type Schwachlastfaehigkeit
+//go:generate jsonenums --type Schwachlastfaehigkeit
+type Schwachlastfaehigkeit int
+
+const (
+	// SCHWACHLASTFAEHIG heißt "true" (EDIFACT Z60)
+	SCHWACHLASTFAEHIG Schwachlastfaehigkeit = iota + 1
+	// NICHT_SCHWACHLASTFAEHIG heißt "false" (EDIFACT Z59)
+	NICHT_SCHWACHLASTFAEHIG
+)
