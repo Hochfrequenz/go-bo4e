eda19998c9ff3918989384d2d169164344db7532
Matthias-René Dachner <matthias-rene.dachner@hochfrequenz.de>
2022-01-14 10:14:22 +0100

    Preisstaffel enhancement

--- com/preisstaffel.go
+++ com/preisstaffel.go
@@ -7,7 +7,7 @@ import (
 // Preisstaffel is part of a Preisposition and includes a price for a distinct set of parameters
 type Preisstaffel struct {
 	Einheitspreis    decimal.Decimal `json:"einheitspreis" validate:"required"` // Preis pro abgerechneter Mengeneinheit
-	StaffelgrenzeVon decimal.Decimal `json:"staffelgrenzevon,omitempty"`        // Unterer Wert, ab dem die Staffel gilt.
-	StaffelgrenzeBis decimal.Decimal `json:"staffelgrenzebis,omitempty"`        // Oberer Wert, bis zu dem die Staffel gilt.
+	StaffelgrenzeVon decimal.Decimal `json:"staffelgrenzeVon,omitempty"`        // Unterer Wert, ab dem die Staffel gilt (inklusiv)
+	StaffelgrenzeBis decimal.Decimal `json:"staffelgrenzeBis,omitempty"`        // Oberer Wert, bis zu dem die Staffel gilt (exklusiv) TODO Inklusivität/Exklusivität noch nicht global abgestimmt!
 	// sigmoidparameter TODO
 }
