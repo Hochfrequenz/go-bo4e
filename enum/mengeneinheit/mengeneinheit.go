d8e2b9d018ec806655c49a14946e03e6c004eba4
konstantin <konstantin.klein@hochfrequenz.de>
2022-01-25 11:38:27 +0100

    Add ANZAHL to Mengeneinheit ENUM (this is redundant) (#86)

--- enum/mengeneinheit/mengeneinheit.go
+++ enum/mengeneinheit/mengeneinheit.go
@@ -15,6 +15,7 @@ const (
 	MW                       // MW is for Megawatt
 	MWH                      // MWH is for Mega watt hour
 	STUECK                   // STUECK is for number of pieces / Stückzahl
+	ANZAHL                   // ANZAHL means the same as STUECK (it has been renamed in the official docs, so this is just a fallback, use STUECK if you can); see also https://github.com/Hochfrequenz/BO4E-python/issues/359
 	KUBIKMETER               // KUBIKMETER is m^3
 	STUNDE                   // STUNDE is 1 hour
 	TAG                      // TAG is 1 day
