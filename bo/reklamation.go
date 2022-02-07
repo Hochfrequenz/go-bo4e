56208e8d330aca022889c85c8690ed644560a67c
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2022-02-07 10:57:56 +0100

    Also rename jsontag

--- bo/reklamation.go
+++ bo/reklamation.go
@@ -11,9 +11,9 @@ type Reklamation struct {
 	Geschaeftsobjekt
 	LokationsId             string                              `json:"lokationsId,omitempty" example:"DE0123456789012345678901234567890" validate:"alphanum,required,min=11,max=33"` // LokationsId is the ID of the location (either a LokationsTyp MALO ID (11 digits) or a LokationsTyp MELO ID (33 alphanum))
 	LokationsTyp            lokationstyp.Lokationstyp           `json:"lokationsTyp,omitempty" example:"MELO" validate:"required"`                                                    // LokationsTyp is the type of the location in LokationsId
-	Reklamationsgrund       reklamationsgrund.Reklamationsgrund `json:"grund,omitempty" validate:"required"`                                                                          // The reason for the complaint
+	Reklamationsgrund       reklamationsgrund.Reklamationsgrund `json:"reklamationsgrund,omitempty" validate:"required"`                                                              // The reason for the complaint
 	ObisKennzahl            *string                             `json:"obisKennzahl,omitempty" validate:"required" example:"1-0:1.8.1"`                                               // Die ObisKennzahl für das Zählwerk, die festlegt, welche auf die gemessene Größe mit dem Stand gemeldet wird. Nur Zählwerkstände mit dieser OBIS-Kennzahl werden an diesem Zählwerk registriert.
-	ZeitraumMesswertanfrage *com.Zeitraum                       `json:"zeitraum,omitempty" validate:"required"`                                                                       // Zeitraum defines the com.Zeitraum where the Energiemenge is complained
+	ZeitraumMesswertanfrage *com.Zeitraum                       `json:"zeitraumMesswertanfrage,omitempty" validate:"required"`                                                        // Zeitraum defines the com.Zeitraum where the Energiemenge is complained
 }
 
 func (_ Reklamation) GetDefaultJsonTags() []string {
