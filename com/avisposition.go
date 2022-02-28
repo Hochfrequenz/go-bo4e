13b857dd69f15959fea768afe8f2e3c7f61cd935
Pascal Mittelstädt <30309020+pmittelstaedt@users.noreply.github.com>
2022-02-28 17:08:25 +0100

    Update com/avisposition.go
    Co-authored-by: konstantin <konstantin.klein@hochfrequenz.de>

--- com/avisposition.go
+++ com/avisposition.go
@@ -5,9 +5,9 @@ import (
 )
 
 type AvisPosition struct {
-	RechnungsNummer string     `json:"rechnungsNummer,omitempty" validate:"required"` // Die Rechnungsnummer der Rechnung, auf die sich das Avis bezieht.
-	RechnungsDatum  time.Time  `json:"rechnungsDatum,omitempty" validate:"required"`  // Das Rechnungsdatum der Rechnung, auf die sich das Avis bezieht.
-	Storno          bool       `json:"istStorno"`                                     // Kennzeichnung, ob es sich bei der Rechnung auf die sich das Avis bezieht, um eine Stornorechnung handelt.
+	RechnungsNummer string     `json:"rechnungsNummer,omitempty" validate:"required"` // Die Rechnungsnummer der Rechnung, auf die sich das Avis bezieht
+	RechnungsDatum  time.Time  `json:"rechnungsDatum,omitempty" validate:"required"`  // Das Rechnungsdatum der Rechnung, auf die sich das Avis bezieht
+	Storno          bool       `json:"istStorno"`                                     // Kennzeichnung, ob es sich bei der Rechnung auf die sich das Avis bezieht, um eine Stornorechnung handelt
 	GesamtBrutto    Betrag     `json:"gesamtBrutto,omitempty" validate:"required"`    // Überweisungsbetrag
 	ZuZahlen        Betrag     `json:"zuZahlen,omitempty" validate:"required"`        // Geforderter Rechnungsbetrag
 	Abweichung      Abweichung `json:"abweichung,omitempty"`                          // Abweichung bei Ablehnung einer COMDIS
