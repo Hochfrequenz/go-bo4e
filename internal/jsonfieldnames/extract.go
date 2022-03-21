3bff807ae575bb24ff7972ff0fb99bd68ffb5849
Timo Reitz <godsboss@gmx.de>
2022-03-21 22:08:33 +0100

    Add function for extracting JSON field names from a value.

---
+++ internal/jsonfieldnames/extract.go
@@ -0,0 +1,98 @@
+// Package jsonfieldnames contains a function to extract JSON field names from a value.
+//
+// In addition to their fixed, hardcoded members, business objects for energy marshal additional values into Geschaeftsobjekt.ExtensionData.
+// As Go's standard library JSON package has no option for a kitchen sink field accepting all remaining JSON fields,
+// there are two passes of unmarshaling: Once into the struct, once into a generic map. To avoid duplicates in the struct and the map,
+// fields that can be found in the struct are removed from the map. This package provides a mechanism to extract the names of
+// these fields in a generic way.
+package jsonfieldnames
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// Extract extracts JSON field names from a struct value or a pointer to a struct value. If the value passed to Extract is nil,
+// is not a struct value or a pointer to a struct value, an error is returned instead.
+func Extract(value interface{}) ([]string, error) {
+	typ := reflect.TypeOf(value)
+	if typ == nil {
+		return nil, fmt.Errorf("cannot extract field names: value is nil")
+	}
+
+	// When value is a pointer, dereference the type, but keep the original type to be used in the error message
+	// in case the dereferenced type is also invalid.
+	originalTyp := typ
+
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+
+	if typ.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("cannot extract field names: value must either be struct nor pointer to a struct, but is %s", originalTyp.String())
+	}
+
+	return extractFromStruct(typ), nil
+}
+
+// extractFromStruct extracts the JSON field names from a struct type. Panics if typ is not a struct type.
+func extractFromStruct(typ reflect.Type) []string {
+	fieldNames := make([]string, 0)
+	fieldCount := typ.NumField()
+
+	// Cycle through all the fields and merge the JSON field names extracted from every field.
+	for i := 0; i < fieldCount; i++ {
+		field := typ.Field(i)
+		fieldNames = append(fieldNames, extractFromField(field)...)
+	}
+
+	return fieldNames
+}
+
+// extractFromField extracts the JSON field names from a struct field. A single field can provide from zero to many JSON field names,
+// because embedded structs are also fields and may contain an arbitrary count of fields themselves.
+func extractFromField(field reflect.StructField) []string {
+	jsonTag := field.Tag.Get("json")
+
+	// If the json tag equals "-", it should not be included at all in the payload.
+	if jsonTag == "-" {
+		return fieldNames()
+	}
+
+	jsonFieldName := jsonFieldNameFromTag(jsonTag)
+
+	switch {
+
+	// field is explicitly given a JSON field name via struct tag.
+	case jsonFieldName != "":
+		return fieldNames(jsonFieldName)
+
+	// field is an embedded struct without explicit JSON field name. That means its fields are merged into the embedding struct.
+	case jsonFieldName == "" && field.Anonymous && field.Type.Kind() == reflect.Struct:
+		return extractFromStruct(field.Type)
+
+	// field is an embedded struct pointer without explicit JSON field name. That means its fields are merged into the embedding struct.
+	case jsonFieldName == "" && field.Anonymous && field.Type.Kind() == reflect.Ptr && field.Type.Elem().Kind() == reflect.Struct:
+		return extractFromStruct(field.Type.Elem())
+
+	// field has no explicit JSON field name, so use the field name instead. Both non-embedded fields and embedded fields that are neither
+	// structs nor pointers to structs are handled this way.
+	default:
+		return fieldNames(field.Name)
+	}
+}
+
+// jsonFieldNameFromTag extracts the desired json field name from a "json" struct tag. The form of these struct tags is "[<name>][,<options>]".
+// This function returns the name part if it is present, else an empty string.
+func jsonFieldNameFromTag(tag string) string {
+	commaPosition := strings.Index(tag, ",")
+	if commaPosition == -1 {
+		return tag
+	}
+	return tag[:commaPosition]
+}
+
+func fieldNames(names ...string) []string {
+	return names
+}
---
+++ internal/jsonfieldnames/extract_test.go
@@ -0,0 +1,162 @@
+package jsonfieldnames_test
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/corbym/gocrest/is"
+	"github.com/corbym/gocrest/then"
+	"github.com/hochfrequenz/go-bo4e/internal/jsonfieldnames"
+)
+
+func TestExtractFromInvalidTypes(t *testing.T) {
+	t.Parallel()
+
+	var stringValue string = "foobar"
+	var intValue int = 42
+	var boolValue bool = true
+
+	inputs := map[string]interface{}{
+		"nil":               nil,
+		"string":            stringValue,
+		"int":               intValue,
+		"bool":              boolValue,
+		"pointer to string": &stringValue,
+		"pointer to int":    &intValue,
+		"pointer to bool":   &boolValue,
+	}
+
+	for name := range inputs {
+		input := inputs[name]
+
+		t.Run(
+			name,
+			func(t *testing.T) {
+				fields, err := jsonfieldnames.Extract(input)
+
+				then.AssertThat(t, fields, is.Nil())
+				then.AssertThat(t, err, is.Not(is.Nil()))
+			},
+		)
+	}
+}
+
+func TestExtractFromValidTypes(t *testing.T) {
+	t.Parallel()
+
+	testcases := map[string]struct {
+		// input is the value that JSON field names are extracted from.
+		input interface{}
+
+		// expectedFields are the fields that we expected to be extracted, in alphabetical order.
+		expectedFields []string
+	}{
+		"struct with no fields": {
+			input:          struct{}{},
+			expectedFields: []string{},
+		},
+		"struct with fields w/o tags": {
+			input: struct {
+				Foo int
+				Bar int
+			}{},
+			expectedFields: []string{"Bar", "Foo"},
+		},
+		"struct with fields with simple tags": {
+			input: struct {
+				Foo int `json:"foo"`
+				Bar int `json:"bar"`
+			}{},
+			expectedFields: []string{"bar", "foo"},
+		},
+		"struct with fields that should be ignored by json package": {
+			input: struct {
+				Foo int `json:"-"`
+				Bar int `json:"-"`
+			}{},
+			expectedFields: []string{},
+		},
+		"struct with fields that have additional options": {
+			input: struct {
+				Foo  int `json:"boo,omitempty"`
+				Bar  int `json:"far,"`
+				Baz  int `json:",omitempty"`
+				Dash int `json:"-,"`
+			}{},
+			expectedFields: []string{"-", "Baz", "boo", "far"},
+		},
+		"pointer to struct": {
+			input: &struct {
+				XYZ int `json:"xyz"`
+			}{},
+			expectedFields: []string{"xyz"},
+		},
+		"struct with embedded structs w/o explit JSON name": {
+			input: struct {
+				embeddedStructHello
+				embeddedStructWorld `json:",omitempty"`
+			}{},
+			expectedFields: []string{"hello", "world"},
+		},
+		"struct with pointers to embedded structs w/o explicit JSON name": {
+			input: struct {
+				*embeddedStructHello
+				*embeddedStructWorld `json:",omitempty"`
+			}{},
+			expectedFields: []string{"hello", "world"},
+		},
+		"struct with embedded structs with tags": {
+			input: struct {
+				embeddedStructHello `json:"embed_1"`
+				embeddedStructWorld `json:"embed_2,omitempty"`
+			}{},
+			expectedFields: []string{"embed_1", "embed_2"},
+		},
+		"struct with embedded non-structs": {
+			input: struct {
+				EmbeddedString
+				EmbeddedInt
+				EmbeddedBool
+				EmbeddedInterface
+			}{},
+			expectedFields: []string{"EmbeddedBool", "EmbeddedInt", "EmbeddedInterface", "EmbeddedString"},
+		},
+	}
+
+	for name := range testcases {
+		testcase := testcases[name]
+
+		t.Run(
+			name,
+			func(t *testing.T) {
+				fields, err := jsonfieldnames.Extract(testcase.input)
+				sort.Strings(fields)
+
+				then.AssertThat(t, fields, is.EqualTo(testcase.expectedFields))
+				then.AssertThat(t, err, is.Nil())
+			},
+		)
+	}
+}
+
+// embeddedStructHello is used as embedded struct in TestExtractFromValidTypes.
+type embeddedStructHello struct {
+	Hello string `json:"hello"`
+}
+
+// embeddedStructWorld is used as embedded struct in TestExtractFromValidTypes.
+type embeddedStructWorld struct {
+	World string `json:"world"`
+}
+
+// EmbeddedString is used as embedded type in TestExtractFromValidTypes.
+type EmbeddedString string
+
+// EmbeddedInt is used as embedded type in TestExtractFromValidTypes.
+type EmbeddedInt int
+
+// EmbeddedBool is used as embedded type in TestExtractFromValidTypes.
+type EmbeddedBool bool
+
+// EmbeddedInterface is used as embedded type in TestExtractFromValidTypes.
+type EmbeddedInterface interface{}
