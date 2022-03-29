3d6356016c5aeaf3e92e9de55ae8f772df224e2e
konstantin <konstantin.klein@hochfrequenz.de>
2022-03-29 11:52:42 +0200

    add docstrings

--- enum/profilart/profilart.go
+++ enum/profilart/profilart.go
@@ -1,12 +1,14 @@
 package profilart
 
-// Profilart describes whether the profil is temperaturabhängig or a standardlastprofil
+// Profilart describes whether the profile is temperaturabhängig or a Standardlastprofil (SLP)
 //go:generate stringer --type Profilart
 //go:generate jsonenums --type Profilart
 type Profilart int
 
+// todo: how and why does this differ from profiltyp.Profiltyp?
+
 const (
-	ART_STANDARDLASTPROFIL Profilart = iota + 1	//Z02
-	ART_TAGESPARAMETERABHAENGIGES_LASTPROFIL    //Z03
-	ART_LASTPROFIL           //Z12
+	ART_STANDARDLASTPROFIL                   Profilart = iota + 1 //ART_STANDARDLASTPROFIL is SLP (Z02 in EDIFACT)
+	ART_TAGESPARAMETERABHAENGIGES_LASTPROFIL                      //ART_TAGESPARAMETERABHAENGIGES_LASTPROFIL is TLP (Z03 in EDIFACT)
+	ART_LASTPROFIL                                                // ART_LASTPROFIL is ??? (Z12 in EDIFACT)
 )
