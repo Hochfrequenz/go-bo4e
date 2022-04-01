db4f3139f8250e9908903f805fae896f4ef19611
Matthias-René Dachner <matthias-rene.dachner@hochfrequenz.de>
2022-04-01 14:24:21 +0200

    Add Value and Scanner to Sparte Enum

--- enum/sparte/sparte.go
+++ enum/sparte/sparte.go
@@ -1,5 +1,9 @@
 package sparte
 
+import (
+	"database/sql/driver"
+)
+
 // Sparte contains different divisions of typical utilities.
 //go:generate stringer --type Sparte
 //go:generate jsonenums --type Sparte
@@ -14,3 +18,24 @@ const (
 	WASSER            // WASSER means water
 	ABWASSER          // ABWASSER means waste water
 )
+
+// Value sets the value that is written to a database, for that we just use the json representation
+func (r Sparte) Value() (driver.Value, error) {
+	v, err := r.MarshalJSON()
+	s := string(v)
+	s = s[1:len(s)-1]
+	return s,err
+}
+
+// Scan - Implement sql scanner interface to read the json representation from the DB
+func (r *Sparte) Scan(value interface{}) error {
+	// if value is nil, false
+	if value == nil {
+		// set the value of the pointer to 0 as default
+		*r = 0
+		return nil
+	}
+	s := *value.(*string)
+	s = "\"" + s + "\""
+	return r.UnmarshalJSON([]byte(s))
+}
---
+++ enum/sparte/sparte_test.go
@@ -0,0 +1,22 @@
+package sparte
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// TestStringifySparteForDB checks if the Value method converts the Sparte to its String representaion and if the Scan reads it
+func TestStringifySparteForDB(t *testing.T) {
+	s := STROM
+
+	v, err := s.Value()
+
+	sb := v.(string)
+
+	var rs Sparte
+	rs.Scan(&sb)
+
+	assert.NoError(t, err)
+	assert.Equal(t,"STROM",sb )
+	assert.Equal(t, STROM,rs)
+}
