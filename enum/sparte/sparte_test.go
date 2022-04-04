1dfa5cd852bb9878d6df10de79df68cf37536de4
konstantin <konstantin.klein@hochfrequenz.de>
2022-04-04 11:45:17 +0200

    more err checks in test case

--- enum/sparte/sparte_test.go
+++ enum/sparte/sparte_test.go
@@ -11,14 +11,13 @@ import (
 // TestStringifySparteForDB checks if the Value method converts the Sparte to its String representaion and if the Scan reads it
 func TestStringifySparteForDB(t *testing.T) {
 	s := STROM
-
 	v, err := s.Value()
 
 	sb := v.(string)
 
 	var rs Sparte
-	rs.Scan(&sb)
-
+	err = rs.Scan(&sb)
+	then.AssertThat(t, err, is.Nil().Reason("No error should occur in this test case"))
 	then.AssertThat(t, err, is.Nil().Reason("No error should occur in this test case"))
 	then.AssertThat(t, sb, is.EqualTo("STROM").Reason("Value() should return the string representation"))
 	then.AssertThat(t, rs, is.EqualTo(STROM).Reason("Scan() result is original Sparte Enum"))
