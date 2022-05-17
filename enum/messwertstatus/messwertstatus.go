2ea2619e5ef279047833b2af0ae942a49b388037
Dominik Korittki <23359034+dkorittki@users.noreply.github.com>
2022-05-17 15:09:40 +0200

    Add Messwertstatus to Verbrauch (#114)
    Adds enum Messwertstatus to Verbrauch + add missing fields to Messwertstatus, which are present in dotnet bo4e library but missing in go-bo4e.

--- enum/messwertstatus/messwertstatus.go
+++ enum/messwertstatus/messwertstatus.go
@@ -6,12 +6,30 @@ package messwertstatus
 type Messwertstatus int
 
 const (
-	// ABGELESEN ist ein Abgelesener Wert (abrechnungsrelevant)
-	ABGELESEN            Messwertstatus = iota + 1
-	ERSATZWERT                          // ERSATZWERT ist ein Ersatzwert - geschätzt, veranschlagt (abrechnungsrelevant)
-	VORSCHLAGSWERT                      // VORSCHLAGSWERT ist ein Vorschlagswert (nicht abrechnungsrelevant)
-	PROGNOSEWERT                        // PROGNOSEWERT ist ein prognostizierter Wert
-	VORLAUFIGERWERT                     // VORLAUFIGERWERT ist ein vorläufiger Wert, dieser kann später angepasst werden.
-	ENERGIEMENGESUMMIERT                // ENERGIEMENGESUMMIERT ist ein Summenwert, Bilanzsumme
-	FEHLT                               // FEHLT ist eine explizite Kennzeichnung eines fehlendn Wertes
+	// ABGELESEN corresponds "Wahrer Wert: 220"
+	ABGELESEN Messwertstatus = iota + 1
+
+	// ERSATZWERT corresponds to"Ersatzwert: 67"
+	ERSATZWERT
+
+	// VOLAEUFIGERWERT corresponds to "Vorläufiger Wert: Z18"
+	VOLAEUFIGERWERT
+
+	// ANGABE_FUER_LIEFERSCHEIN corresponds to "Angabe für Lieferschein: Z31"
+	ANGABE_FUER_LIEFERSCHEIN
+
+	// VORSCHLAGSWERT corresponds to "Vorschlagswert: 201"
+	VORSCHLAGSWERT
+
+	// NICHT_VERWENDBAR corresponds to "Nicht verwendbarer Wert: 20"
+	NICHT_VERWENDBAR
+
+	// PROGNOSEWERT corresponds to "Prognosewert: 187"
+	PROGNOSEWERT
+
+	// ENERGIEMENGESUMMIERT corresponds to "Energiemenge summiert: 79"
+	ENERGIEMENGESUMMIERT
+
+	// FEHLT corresponds to "Fehlender Wert: Z30"
+	FEHLT
 )
