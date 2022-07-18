91aaf671794dd59922597e99d1a73dcb4a0ad351
Dominik Korittki <23359034+dkorittki@users.noreply.github.com>
2022-07-18 14:14:32 +0200

    Implement Statuszusatzinformationen for component Verbrauch (#133)

--- com/verbrauch.go
+++ com/verbrauch.go
@@ -22,6 +22,11 @@ type Verbrauch struct {
 	// Messwertstatus includes the plausibility of the value
 	Messwertstatus *messwertstatus.Messwertstatus `json:"messwertstatus,omitempty" validate:"omitempty"`
 
+	// StatusZusatzInformationen enthält die Auflistung der STS Segmente
+	// Plausibilisierungshinweis, Ersatzwertbildungsverfahren, Korrekturgrund,
+	// Gasqualität, Tarif, Grundlage der Energiemenge.
+	StatusZusatzInformationen []StatusZusatzInformation `json:"statuszusatzinformationen" validate:"omitempty"`
+
 	// Obiskennzahl is the OBIS Kennzahl
 	// (?<power>(1)-((?:[0-5]?[0-9])|(?:6[0-5])):((?:[1-8]|99))\.((?:6|8|9|29))\.([0-9]{1,2}))||(?<gas>)(7)-((?:[0-5]?[0-9])|(?:6[0-5])):(.{1,2})\.(.{1,2})\.([0-9]{1,2})
 	Obiskennzahl string `json:"obiskennzahl,omitempty" validate:"required" example:"1-0:1.8.1"`
