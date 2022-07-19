bb7cf5212ec73ebd8b39ce97f365f9ce2258b5e4
rmorlock <54396093+rmorlock@users.noreply.github.com>
2022-07-19 10:03:47 +0200

    Expand lib for PRICAT (#132)
    * add enum aufabschlagstyp
    * expand enum bdewartikelnummer
    * add enum leistungsbezeichnung
    * add start-/enddatum to zeitraum com
    * expand preisposition com for pricat
    * add positionsaufabschlag com
    * update adjustment in bdewartikelnummer
    * run go generate
    * update to not nullable in positionsaufabschlag
    * add omitempty to values in preisposition

--- com/zeitraum.go
+++ com/zeitraum.go
@@ -10,6 +10,8 @@ import (
 type Zeitraum struct {
 	Einheit        zeiteinheit.Zeiteinheit `json:"zeiteinheit,omitempty" validate:"omitempty,required_with=Dauer"`                                                                   // Einheit is the unit of measurement
 	Dauer          decimal.NullDecimal     `json:"dauer,omitempty" validate:"omitempty,required_with=Einheit"`                                                                       // Dauer ist eh duration
+	Startdatum     time.Time               `json:"startdatum,omitempty" validate:"required_without_all=Einheit Dauer" example:"2018-01-28T10:15:00+01"`                              // Startdatum is the day and time of begin
+	Enddatum       time.Time               `json:"enddatum,omitempty" validate:"required_without_all=Einheit Dauer" example:"2018-01-28T10:15:00+01"`                                // Enddatum is the day and time of end
 	Startzeitpunkt time.Time               `json:"startzeitpunkt,omitempty" validate:"required_without_all=Einheit Dauer" example:"2018-01-28T10:15:00+01"`                          // Startzeitpunkt is the inclusive begin
 	Endzeitpunkt   time.Time               `json:"endzeitpunkt,omitempty" validate:"required_with=Startzeitpunkt,omitempty,gtfield=Startzeitpunkt" example:"2018-01-28T10:30:00+01"` // Endzeitpunkt is the exclusive end
 }
