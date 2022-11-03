e10b618eebd9d625f8dc356ec6a957648bbb5c19
Annika Schlögl <annika.schloegl@hochfrequenz.de>
2022-11-03 08:43:15 +0100

    Make a list out of Abweichung(en)

--- com/avisposition.go
+++ com/avisposition.go
@@ -11,6 +11,6 @@ type AvisPosition struct {
 	IstSelbstausgestellt *bool                   `json:"istSelbstausgestellt,omitempty"`                // Kennzeichnung, ob es sich bei der Rechnung auf die sich das Avis bezieht, um eine selbst aus gestellte Rechnung handelt
 	GesamtBrutto         Betrag                  `json:"gesamtBrutto,omitempty" validate:"required"`    // Überweisungsbetrag
 	ZuZahlen             Betrag                  `json:"zuZahlen,omitempty" validate:"required"`        // Geforderter Rechnungsbetrag
-	Abweichung           *Abweichung             `json:"abweichung,omitempty"`                          // Abweichung ist der Grund der Ablehnung einer INVOIC oder COMDIS
+	Abweichungen         []Abweichung            `json:"abweichungen,omitempty"`                        // Abweichungen ist eine Liste der Gründe der Ablehnung einer INVOIC oder COMDIS
 	Positionen           []Rueckmeldungsposition `json:"positionen,omitempty"`                          // Rückmeldungspositionen
 }
