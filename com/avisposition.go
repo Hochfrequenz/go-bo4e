8cacdffb2a9617c3d00366fd2ee70c19a2f7b903
Annika <73470827+hf-aschloegl@users.noreply.github.com>
2022-11-03 13:44:10 +0100

    ED4FTR-31248: Move Rückmeldungsposition and make a list out of Abweichung(en) (#176)
    * Move Rückmeldungsposition from Abweichung to Avisposition
    
    * Make a list out of Abweichung(en)
    
    * Adapt tests and validation to new structure
    
    * Check for empty list Abweichungen
    
    * Add test for empty list Abweichungen
    
    * Also empty list check in second if condition

--- com/avisposition.go
+++ com/avisposition.go
@@ -5,11 +5,12 @@ import (
 )
 
 type AvisPosition struct {
-	RechnungsNummer      string      `json:"rechnungsNummer,omitempty" validate:"required"` // Die Rechnungsnummer der Rechnung, auf die sich das Avis bezieht
-	RechnungsDatum       time.Time   `json:"rechnungsDatum,omitempty" validate:"required"`  // Das Rechnungsdatum der Rechnung, auf die sich das Avis bezieht
-	Storno               bool        `json:"istStorno"`                                     // Kennzeichnung, ob es sich bei der Rechnung auf die sich das Avis bezieht, um eine Stornorechnung handelt
-	IstSelbstausgestellt *bool       `json:"istSelbstausgestellt,omitempty"`                // Kennzeichnung, ob es sich bei der Rechnung auf die sich das Avis bezieht, um eine selbst aus gestellte Rechnung handelt
-	GesamtBrutto         Betrag      `json:"gesamtBrutto,omitempty" validate:"required"`    // Überweisungsbetrag
-	ZuZahlen             Betrag      `json:"zuZahlen,omitempty" validate:"required"`        // Geforderter Rechnungsbetrag
-	Abweichung           *Abweichung `json:"abweichung,omitempty"`                          // Abweichung ist der Grund der Ablehnung einer INVOIC oder COMDIS
+	RechnungsNummer      string                  `json:"rechnungsNummer,omitempty" validate:"required"` // Die Rechnungsnummer der Rechnung, auf die sich das Avis bezieht
+	RechnungsDatum       time.Time               `json:"rechnungsDatum,omitempty" validate:"required"`  // Das Rechnungsdatum der Rechnung, auf die sich das Avis bezieht
+	Storno               bool                    `json:"istStorno"`                                     // Kennzeichnung, ob es sich bei der Rechnung auf die sich das Avis bezieht, um eine Stornorechnung handelt
+	IstSelbstausgestellt *bool                   `json:"istSelbstausgestellt,omitempty"`                // Kennzeichnung, ob es sich bei der Rechnung auf die sich das Avis bezieht, um eine selbst aus gestellte Rechnung handelt
+	GesamtBrutto         Betrag                  `json:"gesamtBrutto,omitempty" validate:"required"`    // Überweisungsbetrag
+	ZuZahlen             Betrag                  `json:"zuZahlen,omitempty" validate:"required"`        // Geforderter Rechnungsbetrag
+	Abweichungen         []Abweichung            `json:"abweichungen,omitempty"`                        // Abweichungen ist eine Liste der Gründe der Ablehnung einer INVOIC oder COMDIS
+	Positionen           []Rueckmeldungsposition `json:"positionen,omitempty"`                          // Rückmeldungspositionen
 }
