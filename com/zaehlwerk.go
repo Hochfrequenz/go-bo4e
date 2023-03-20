ffe6266aaf46c696c0bf3107a50e55eea7324847
qndaa <qndaa@uns.ac.rs>
2023-03-20 10:41:43 +0100

    new fields Unterbrechbarkeit and Waermenutzung inside of Zaehlwerk struct

--- com/zaehlwerk.go
+++ com/zaehlwerk.go
@@ -4,21 +4,25 @@ import (
 	"github.com/hochfrequenz/go-bo4e/enum/energierichtung"
 	"github.com/hochfrequenz/go-bo4e/enum/mengeneinheit"
 	"github.com/hochfrequenz/go-bo4e/enum/schwachlastfaehigkeit"
+	"github.com/hochfrequenz/go-bo4e/enum/unterbrechbarkeit"
 	"github.com/hochfrequenz/go-bo4e/enum/verbrauchsart"
+	"github.com/hochfrequenz/go-bo4e/enum/waermenutzung"
 	"github.com/shopspring/decimal"
 )
 
 // A Zaehlwerk is the counting part of a meter. A meter consists of one or more Zaehlwerke
 type Zaehlwerk struct {
-	ZaehlwerkId       string                          `json:"zaehlwerkId,omitempty" validate:"required" example:"47110815_1"`          // ZaehlwerkId ist die Identifikation des Zählwerks (Registers) innerhalb des Zählers. Oftmals eine laufende Nummer hinter der Zählernummer.
-	Bezeichnung       string                          `json:"bezeichnung,omitempty" validate:"required" example:"Zählwerk_Wirkarbeit"` // Bezeichnung ist eine zusätzliche Bezeichnung
-	Richtung          energierichtung.Energierichtung `json:"richtung,omitempty" validate:"required"`                                  // Richtung beschreibt die Energierichtung: Einspeisung oder Ausspeisung.
-	ObisKennzahl      string                          `json:"obisKennzahl,omitempty" validate:"required" example:"1-0:1.8.1"`          // Die ObisKennzahl für das Zählwerk, die festlegt, welche auf die gemessene Größe mit dem Stand gemeldet wird. Nur Zählwerkstände mit dieser OBIS-Kennzahl werden an diesem Zählwerk registriert.
-	Wandlerfaktor     decimal.Decimal                 `json:"wandlerfaktor" validate:"required"`                                       // Der Wandlerfaktor ist der Faktor, mit dem die Zählerstandsdifferenz multipliziert wird, um zum eigentlichen Verbrauch im Zeitraum zu kommen.
-	Einheit           mengeneinheit.Mengeneinheit     `json:"einheit,omitempty" validate:"required"`                                   // Die Einheit der gemessenen Größe
-	Zaehlerstaende    Zaehlerstaende                  `json:"zaehlerstaende,omitempty"`                                                // A list of Zaehlerstand (Non BO4E Standard)
-	Verwendungszwecke []Verwendungszweck              `json:"verwendungszwecke,omitempty"`                                             // Verwendungungszweck der Werte Marktlokation
-	Verbrauchsart     *verbrauchsart.Verbrauchsart    `json:"verbrauchsart,omitempty"`                                                 // Stromverbrauchsart/Verbrauchsart Marktlokation
+	ZaehlwerkId       string                               `json:"zaehlwerkId,omitempty" validate:"required" example:"47110815_1"`          // ZaehlwerkId ist die Identifikation des Zählwerks (Registers) innerhalb des Zählers. Oftmals eine laufende Nummer hinter der Zählernummer.
+	Bezeichnung       string                               `json:"bezeichnung,omitempty" validate:"required" example:"Zählwerk_Wirkarbeit"` // Bezeichnung ist eine zusätzliche Bezeichnung
+	Richtung          energierichtung.Energierichtung      `json:"richtung,omitempty" validate:"required"`                                  // Richtung beschreibt die Energierichtung: Einspeisung oder Ausspeisung.
+	ObisKennzahl      string                               `json:"obisKennzahl,omitempty" validate:"required" example:"1-0:1.8.1"`          // Die ObisKennzahl für das Zählwerk, die festlegt, welche auf die gemessene Größe mit dem Stand gemeldet wird. Nur Zählwerkstände mit dieser OBIS-Kennzahl werden an diesem Zählwerk registriert.
+	Wandlerfaktor     decimal.Decimal                      `json:"wandlerfaktor" validate:"required"`                                       // Der Wandlerfaktor ist der Faktor, mit dem die Zählerstandsdifferenz multipliziert wird, um zum eigentlichen Verbrauch im Zeitraum zu kommen.
+	Einheit           mengeneinheit.Mengeneinheit          `json:"einheit,omitempty" validate:"required"`                                   // Die Einheit der gemessenen Größe
+	Zaehlerstaende    Zaehlerstaende                       `json:"zaehlerstaende,omitempty"`                                                // A list of Zaehlerstand (Non BO4E Standard)
+	Verwendungszwecke []Verwendungszweck                   `json:"verwendungszwecke,omitempty"`                                             // Verwendungungszweck der Werte Marktlokation
+	Verbrauchsart     *verbrauchsart.Verbrauchsart         `json:"verbrauchsart,omitempty"`                                                 // Stromverbrauchsart/Verbrauchsart Marktlokation
+	Unterbrechbarkeit *unterbrechbarkeit.Unterbrechbarkeit `json:"unterbrechbarkeit,omitempty"`                                             // Stromverbrauchsart/Unterbrechbarkeit Marktlokation
+	Waermenutzung     *waermenutzung.Waermenutzung         `json:"waermenutzung,omitempty"`                                                 // Stromverbrauchsart/Wärmenutzung Marktlokation
 	// The following fields are non bo4e standard (yet)
 
 	// note that both "kommastellen" (plural) serialize as singular "...stelle" for compatability reasons (with bo4e dotnet)
