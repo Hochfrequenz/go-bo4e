0acef7214705167c85b54654e5ceb42ae739fe01
Daniel <139119540+hf-ddernbach@users.noreply.github.com>
2023-09-13 15:56:42 +0200

    validator v10 bump 10.14.1 10.15.4 (#245)
    * Bump github.com/go-playground/validator/v10 from 10.14.1 to 10.15.4
    
    Bumps [github.com/go-playground/validator/v10](https://github.com/go-playground/validator) from 10.14.1 to 10.15.4.
    - [Release notes](https://github.com/go-playground/validator/releases)
    - [Commits](https://github.com/go-playground/validator/compare/v10.14.1...v10.15.4)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/go-playground/validator/v10
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * :adhesive_bandage: changed type for geokoordinaten -> validation tag
    
    * Mitigate validator for geolocation to maintain decimal.Decimal type
    
    * removed todos
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- com/geokoordinaten.go
+++ com/geokoordinaten.go
@@ -4,6 +4,7 @@ import "github.com/shopspring/decimal"
 
 // Geokoordinaten are GPS coordinates
 type Geokoordinaten struct {
-	Breitengrad decimal.Decimal `json:"breitengrad" validate:"latitude"`  // Breitengrad is the latitude
-	Laengengrad decimal.Decimal `json:"laengengrad" validate:"longitude"` // Laengengrad is the longitude
+	Breitengrad decimal.Decimal `json:"breitengrad" validate:"required"` // Breitengrad is the latitude
+	Laengengrad decimal.Decimal `json:"laengengrad" validate:"required"` // Laengengrad is the longitude
+	// todo: change to decimal.Decimal -> validator latitude/longitude lte=-90,gte=90/lte=-180,gte=180 won't work
 }
