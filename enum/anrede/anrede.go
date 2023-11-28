dff8ef1c9b4586137b0f4e826f68692a15598692
Timo Reitz <timo.reitz@lynq.tech>
2023-11-28 09:10:01 +0100

    Extend Anrede enum with unofficial values taken from .NET package (#253)
    * Add Anrede values as in .NET package.
    
    * Mark unofficial values in enum as such.
    
    * Mark DIVERS as unofficial as well.

--- enum/anrede/anrede.go
+++ enum/anrede/anrede.go
@@ -8,11 +8,15 @@ type Anrede int
 
 const (
 	// HERR (male)
-	HERR             Anrede = iota + 1
-	FRAU                    // FRAU (female)
-	DIVERS                  // DIVERS (non-binary)
-	EHELEUTE                // EHELEUTE (married couple)
-	FIRMA                   // FIRMA (company)
-	WOHNGEMEINSCHAFT        // WOHNGEMEINSCHAFT is a share flat/WG
-	INDIVIDUELL             // INDIVIDUELL festgelegt
+	HERR                    Anrede = iota + 1
+	FRAU                           // FRAU (female)
+	DIVERS                         // DIVERS (non-binary) - Unofficial / Customer requirement, not part of the BO4E standard yet.
+	EHELEUTE                       // EHELEUTE (married couple)
+	FIRMA                          // FIRMA (company)
+	WOHNGEMEINSCHAFT               // WOHNGEMEINSCHAFT is a share flat/WG - Unofficial / Customer requirement, not part of the BO4E standard yet.
+	INDIVIDUELL                    // INDIVIDUELL festgelegt
+	FAMILIE                        // FAMILIE (family) - Unofficial / Customer requirement, not part of the BO4E standard yet.
+	ERBENGEMEINSCHAFT              // ERBENGEMEINSCHAFT (community of heirs, community of co-heirs, community of joint heirs) - Unofficial / Customer requirement, not part of the BO4E standard yet.
+	GRUNDSTUECKGEMEINSCHAFT        // GRUNDSTUECKGEMEINSCHAFT - Unofficial / Customer requirement, not part of the BO4E standard yet.
+	DR                             // DR (doctor) - Unofficial / Customer requirement, not part of the BO4E standard yet.
 )
