3fe84776fd4d8efe078d104dc509f88a46a0d98b
Timo Reitz <godsboss@gmx.de>
2024-01-27 16:47:59 +0100

    Remove most suite usages from Statuszusatzinformation tests.

--- com/statuszusatzinformation_test.go
+++ com/statuszusatzinformation_test.go
@@ -2,17 +2,19 @@ package com_test
 
 import (
 	"encoding/json"
+	"strings"
+	"testing"
+
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
 	"github.com/go-playground/validator/v10"
 	"github.com/hochfrequenz/go-bo4e/com"
 	"github.com/hochfrequenz/go-bo4e/enum/status"
 	"github.com/hochfrequenz/go-bo4e/enum/statusart"
-	"strings"
 )
 
 // Test_StatusZusatzInformation_Deserialization deserializes an StatusZusatzInformation json
-func (s *Suite) Test_StatusZusatzInformation_Deserialization() {
+func Test_StatusZusatzInformation_Deserialization(t *testing.T) {
 	gasQualitaetArt := statusart.GASQUALITAET
 	var sts = com.StatusZusatzInformation{
 		Art:    &gasQualitaetArt,
@@ -21,19 +23,19 @@ func (s *Suite) Test_StatusZusatzInformation_Deserialization() {
 
 	serializedSts, err := json.Marshal(sts)
 	jsonString := string(serializedSts)
-	then.AssertThat(s.T(), strings.Contains(jsonString, "GASQUALITAET"), is.True())
-	then.AssertThat(s.T(), strings.Contains(jsonString, "BRENNWERTKORREKTUR"), is.True())
-	then.AssertThat(s.T(), err, is.Nil())
-	then.AssertThat(s.T(), serializedSts, is.Not(is.NilArray[byte]()))
+	then.AssertThat(t, strings.Contains(jsonString, "GASQUALITAET"), is.True())
+	then.AssertThat(t, strings.Contains(jsonString, "BRENNWERTKORREKTUR"), is.True())
+	then.AssertThat(t, err, is.Nil())
+	then.AssertThat(t, serializedSts, is.Not(is.NilArray[byte]()))
 	var deserializedSts com.StatusZusatzInformation
 	err = json.Unmarshal(serializedSts, &deserializedSts)
-	then.AssertThat(s.T(), err, is.Nil())
-	then.AssertThat(s.T(), deserializedSts, is.EqualTo(sts))
+	then.AssertThat(t, err, is.Nil())
+	then.AssertThat(t, deserializedSts, is.EqualTo(sts))
 }
 
 // Test_Successful_StatusZusatzInformation_Validation asserts that the validation
 // does not fail for a valid StatusZusatzInformation
-func (s *Suite) Test_Successful_StatusZusatzInformation_Validation() {
+func Test_Successful_StatusZusatzInformation_Validation(t *testing.T) {
 	statusartGasqualitaet := statusart.GASQUALITAET
 	validate := validator.New()
 	validSts := []interface{}{
@@ -45,12 +47,12 @@ func (s *Suite) Test_Successful_StatusZusatzInformation_Validation() {
 			Status: status.BRENNWERTKORREKTUR,
 		},
 	}
-	VerfiySuccessfulValidations(s, validate, validSts)
+	VerifySuccessfulValidations(t, validate, validSts)
 }
 
 // Test_StatusZusatzInformation_FailedValidation verifies that invalid
 // StatusZusatzInformation values are considered invalid
-func (s *Suite) Test_StatusZusatzInformation_FailedValidation() {
+func Test_StatusZusatzInformation_FailedValidation(t *testing.T) {
 	statusartGasqualitaet := statusart.GASQUALITAET
 	validate := validator.New()
 	invalidVerbrauchMap := map[string][]interface{}{
@@ -60,7 +62,7 @@ func (s *Suite) Test_StatusZusatzInformation_FailedValidation() {
 			},
 		},
 	}
-	VerfiyFailedValidations(s, validate, invalidVerbrauchMap)
+	VerifyFailedValidations(t, validate, invalidVerbrauchMap)
 }
 
 func (s *Suite) Test_Serialized_Empty_StatusZusatzInformation_Contains_No_Enum_Defaults() {
