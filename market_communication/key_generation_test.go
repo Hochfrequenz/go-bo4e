3c490f4d9dd64581eb9298f2ab8fac3d51346975
Timo Reitz <godsboss@gmx.de>
2024-01-27 16:24:07 +0100

    Remove suite usage from key generation tests.

--- market_communication/key_generation_test.go
+++ market_communication/key_generation_test.go
@@ -1,23 +1,25 @@
 package market_communication_test
 
 import (
+	"regexp"
+	"testing"
+
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
 	"github.com/hochfrequenz/go-bo4e/market_communication"
-	"regexp"
 )
 
 var expectedBgmPattern = regexp.MustCompile(`[A-Z\d]{35}`)
 var expectedUnhPattern = regexp.MustCompile(`[A-Z\d]{14}`)
 
-func (s *Suite) Test_GenerationRandomDokumentennummer() {
+func Test_GenerationRandomDokumentennummer(t *testing.T) {
 	actual := market_communication.GenerateRandomDokumentennummer()
-	then.AssertThat(s.T(), expectedBgmPattern.MatchString(actual), is.True())
+	then.AssertThat(t, expectedBgmPattern.MatchString(actual), is.True())
 }
 
-func (s *Suite) Test_GenerationRandomNachrichtenReferenznummer() {
+func Test_GenerationRandomNachrichtenReferenznummer(t *testing.T) {
 	actual := market_communication.GenerateRandomNachrichtenReferenznummer()
-	then.AssertThat(s.T(), expectedUnhPattern.MatchString(actual), is.True())
+	then.AssertThat(t, expectedUnhPattern.MatchString(actual), is.True())
 }
 
 const numberOfTries = 1000 // <-- the higher the more "random" the functions return values are
@@ -31,10 +33,10 @@ func funcGeneratesDuplicates(stringGenerator func() string) bool {
 	return len(set) != numberOfTries
 }
 
-func (s *Suite) Test_RandomDokumentennummer_Is_Random_Enough() {
-	then.AssertThat(s.T(), funcGeneratesDuplicates(market_communication.GenerateRandomDokumentennummer), is.False())
+func Test_RandomDokumentennummer_Is_Random_Enough(t *testing.T) {
+	then.AssertThat(t, funcGeneratesDuplicates(market_communication.GenerateRandomDokumentennummer), is.False())
 }
 
-func (s *Suite) Test_RandomNachrichtenReferenznummer_Is_Random_Enough() {
-	then.AssertThat(s.T(), funcGeneratesDuplicates(market_communication.GenerateRandomNachrichtenReferenznummer), is.False())
+func Test_RandomNachrichtenReferenznummer_Is_Random_Enough(t *testing.T) {
+	then.AssertThat(t, funcGeneratesDuplicates(market_communication.GenerateRandomNachrichtenReferenznummer), is.False())
 }
