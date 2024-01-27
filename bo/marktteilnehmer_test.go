f67b199798eea823a2fbc2d3825886efb6881e2f
Timo Reitz <godsboss@gmx.de>
2024-01-27 15:54:18 +0100

    Remove most suite usages from Marktteilnehmer tests.

--- bo/marktteilnehmer_test.go
+++ bo/marktteilnehmer_test.go
@@ -2,9 +2,11 @@ package bo_test
 
 import (
 	"encoding/json"
-	"github.com/hochfrequenz/go-bo4e/internal"
 	"reflect"
 	"strings"
+	"testing"
+
+	"github.com/hochfrequenz/go-bo4e/internal"
 
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
@@ -22,7 +24,7 @@ import (
 )
 
 // Test_Marktteilnehmer_Deserialization deserializes an Marktteilnehmer json
-func (s *Suite) Test_Marktteilnehmer_Deserialization() {
+func Test_Marktteilnehmer_Deserialization(t *testing.T) {
 	var mt = bo.Marktteilnehmer{
 		Marktrolle:       marktrolle.LF,
 		Makoadresse:      "edifact@my-favourite-marketpartner.de",
@@ -74,18 +76,18 @@ func (s *Suite) Test_Marktteilnehmer_Deserialization() {
 	}
 	serializedMarktteilnehmer, err := json.Marshal(mt)
 	jsonString := string(serializedMarktteilnehmer)
-	then.AssertThat(s.T(), strings.Contains(jsonString, "DVGW"), is.True()) // stringified enum
-	then.AssertThat(s.T(), strings.Contains(jsonString, "LF"), is.True())   // stringified enum
-	then.AssertThat(s.T(), err, is.Nil())
-	then.AssertThat(s.T(), serializedMarktteilnehmer, is.Not(is.NilArray[byte]()))
+	then.AssertThat(t, strings.Contains(jsonString, "DVGW"), is.True()) // stringified enum
+	then.AssertThat(t, strings.Contains(jsonString, "LF"), is.True())   // stringified enum
+	then.AssertThat(t, err, is.Nil())
+	then.AssertThat(t, serializedMarktteilnehmer, is.Not(is.NilArray[byte]()))
 	var deserializedMarktteilnehmer bo.Marktteilnehmer
 	err = json.Unmarshal(serializedMarktteilnehmer, &deserializedMarktteilnehmer)
-	then.AssertThat(s.T(), err, is.Nil())
-	then.AssertThat(s.T(), deserializedMarktteilnehmer, is.EqualTo(mt))
+	then.AssertThat(t, err, is.Nil())
+	then.AssertThat(t, deserializedMarktteilnehmer, is.EqualTo(mt))
 }
 
 // Test_Failed_Marktteilnehmer_Validation verifies that the validators of a Marktteilnehmer work
-func (s *Suite) Test_Failed_Marktteilnehmer_Validation() {
+func Test_Failed_Marktteilnehmer_Validation(t *testing.T) {
 	validate := validator.New()
 	invalidMarktteilnehmer := map[string][]interface{}{
 		"required": {
@@ -101,11 +103,11 @@ func (s *Suite) Test_Failed_Marktteilnehmer_Validation() {
 			bo.Marktteilnehmer{Rollencodenummer: "asd"},
 		},
 	}
-	VerfiyFailedValidations(s, validate, invalidMarktteilnehmer)
+	VerifyFailedValidations(t, validate, invalidMarktteilnehmer)
 }
 
 // Test_Successful_Marktteilnehmer_Validation verifies that a valid BO is validated without errors
-func (s *Suite) Test_Successful_Marktteilnehmer_Validation() {
+func Test_Successful_Marktteilnehmer_Validation(t *testing.T) {
 	validate := validator.New()
 	validMarktteilnehmers := []bo.BusinessObject{
 		bo.Marktteilnehmer{
@@ -134,15 +136,15 @@ func (s *Suite) Test_Successful_Marktteilnehmer_Validation() {
 			},
 		},
 	}
-	VerfiySuccessfulValidations(s, validate, validMarktteilnehmers)
+	VerifySuccessfulValidations(t, validate, validMarktteilnehmers)
 }
 
-func (s *Suite) Test_Empty_Markteilnehmer_Is_Creatable_Using_BoTyp() {
+func Test_Empty_Markteilnehmer_Is_Creatable_Using_BoTyp(t *testing.T) {
 	object := bo.NewBusinessObject(botyp.MARKTTEILNEHMER)
-	then.AssertThat(s.T(), object, is.Not(is.EqualTo[bo.BusinessObject](nil)))
-	then.AssertThat(s.T(), reflect.TypeOf(object), is.EqualTo(reflect.TypeOf(&bo.Marktteilnehmer{})))
-	then.AssertThat(s.T(), object.GetBoTyp(), is.EqualTo(botyp.MARKTTEILNEHMER))
-	then.AssertThat(s.T(), object.GetVersionStruktur(), is.EqualTo("1.1"))
+	then.AssertThat(t, object, is.Not(is.EqualTo[bo.BusinessObject](nil)))
+	then.AssertThat(t, reflect.TypeOf(object), is.EqualTo(reflect.TypeOf(&bo.Marktteilnehmer{})))
+	then.AssertThat(t, object.GetBoTyp(), is.EqualTo(botyp.MARKTTEILNEHMER))
+	then.AssertThat(t, object.GetVersionStruktur(), is.EqualTo("1.1"))
 }
 
 func (s *Suite) Test_Serialized_Empty_Marktteilnehmer_Contains_No_Enum_Defaults() {
