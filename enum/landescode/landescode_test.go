b17e792194a49aebde75991dbed5d3bc71359ccc
Timo Reitz <godsboss@gmx.de>
2024-01-27 16:32:01 +0100

    Remove suite usage from Landescode tests.

--- enum/landescode/landescode_test.go
+++ enum/landescode/landescode_test.go
@@ -7,18 +7,9 @@ import (
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
 	"github.com/hochfrequenz/go-bo4e/enum/landescode"
-	"github.com/stretchr/testify/suite"
 )
 
-type Suite struct {
-	suite.Suite
-}
-
 // Test_Landescode_ToString deserializes an address json
-func (s *Suite) Test_Landescode_ToString() {
-	then.AssertThat(s.T(), fmt.Sprintf("%v", landescode.DE), is.EqualTo("DE"))
-}
-
-func TestInit(t *testing.T) {
-	suite.Run(t, new(Suite))
+func Test_Landescode_ToString(t *testing.T) {
+	then.AssertThat(t, fmt.Sprintf("%v", landescode.DE), is.EqualTo("DE"))
 }
