5eafa8d1f26409d563d7b585cee0578f652a24db
Timo Reitz <godsboss@gmx.de>
2024-01-27 15:56:20 +0100

    Remove suite usage from all Netzlokation tests.

--- bo/netzlokation_test.go
+++ bo/netzlokation_test.go
@@ -1,17 +1,19 @@
 package bo_test
 
 import (
+	"testing"
+
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
 	"github.com/hochfrequenz/go-bo4e/bo"
 )
 
-func (s *Suite) Test_Get_NeloId_Checksum() {
+func Test_Get_NeloId_Checksum(t *testing.T) {
 	actual := bo.GetNeLoIdCheckSum("E113735592")
-	then.AssertThat(s.T(), actual, is.EqualTo(1))
+	then.AssertThat(t, actual, is.EqualTo(1))
 }
 
-func (s *Suite) Test_Get_NeloId_Doesnt_Panic() {
+func Test_Get_NeloId_Doesnt_Panic(t *testing.T) {
 	actual := bo.GetNeLoIdCheckSum("E5345G7F6F")
-	then.AssertThat(s.T(), actual, is.EqualTo(0))
+	then.AssertThat(t, actual, is.EqualTo(0))
 }
