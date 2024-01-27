6ac7f1ed90c51c8b99fab01e923a3a22de320885
Timo Reitz <godsboss@gmx.de>
2024-01-27 15:29:14 +0100

    Provide functions for directly checking against *testing.T instead of testsuite.

--- bo/setup_test.go
+++ bo/setup_test.go
@@ -23,24 +23,32 @@ func TestInit(t *testing.T) {
 
 // VerfiySuccessfulValidations asserts that the vali validator does not fail for all objects provided
 func VerfiySuccessfulValidations(s *Suite, vali *validator.Validate, validObjects []bo.BusinessObject) {
+	VerifySuccessValidations(s.T(), vali, validObjects)
+}
+
+func VerifySuccessValidations(t *testing.T, vali *validator.Validate, validObjects []bo.BusinessObject) {
 	// ToDo: use generics as soon as golangs allows to
 	for _, validObject := range validObjects {
 		err := vali.Struct(validObject)
-		then.AssertThat(s.T(), validObject.GetBoTyp(), is.Not(is.EqualTo[botyp.BOTyp](0))) // 0 would be iota/uninitialized botyp.BOTyp
-		then.AssertThat(s.T(), err, is.Nil())
+		then.AssertThat(t, validObject.GetBoTyp(), is.Not(is.EqualTo[botyp.BOTyp](0))) // 0 would be iota/uninitialized botyp.BOTyp
+		then.AssertThat(t, err, is.Nil())
 	}
 }
 
 // VerfiyFailedValidations asserts that the vali validator fails with the expected tag for every object
 func VerfiyFailedValidations(s *Suite, vali *validator.Validate, tagInvalidObjectsMap map[string][]interface{}) {
+	VerifyFailedValidations(s.T(), vali, tagInvalidObjectsMap)
+}
+
+func VerifyFailedValidations(t *testing.T, vali *validator.Validate, tagInvalidObjectsMap map[string][]interface{}) {
 	// ToDo: use generics as soon as golangs allows to
 	for validationTag, invalidObjects := range tagInvalidObjectsMap {
 		for _, invalidObject := range invalidObjects {
 			err := vali.Struct(invalidObject)
-			then.AssertThat(s.T(), err, is.Not(is.Nil()))
+			then.AssertThat(t, err, is.Not(is.Nil()))
 			tagFound := false
 			for _, validationError := range err.(validator.ValidationErrors) {
-				then.AssertThat(s.T(), validationError, is.Not(is.EqualTo[validator.FieldError](nil)))
+				then.AssertThat(t, validationError, is.Not(is.EqualTo[validator.FieldError](nil)))
 				// sometimes there's more than one tag/validation error
 				if validationError.Tag() == validationTag {
 					tagFound = true
@@ -48,9 +56,9 @@ func VerfiyFailedValidations(s *Suite, vali *validator.Validate, tagInvalidObjec
 				}
 			}
 			if !tagFound {
-				then.AssertThat(s.T(), validationTag, is.EmptyString())
+				then.AssertThat(t, validationTag, is.EmptyString())
 			}
-			then.AssertThat(s.T(), tagFound, is.True())
+			then.AssertThat(t, tagFound, is.True())
 		}
 	}
 }
