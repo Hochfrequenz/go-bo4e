4dc5ad6ada20dae90d324b3511d744d26d55eeae
Timo Reitz <godsboss@gmx.de>
2024-01-27 16:54:37 +0100

    Remove suite usage from Zaehlerstaende tests.

--- com/zaehlerstaende_test.go
+++ com/zaehlerstaende_test.go
@@ -1,31 +1,33 @@
 package com_test
 
 import (
+	"testing"
+	"time"
+
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
 	"github.com/hochfrequenz/go-bo4e/com"
 	"github.com/hochfrequenz/go-bo4e/enum/mengeneinheit"
 	"github.com/hochfrequenz/go-bo4e/enum/wertermittlungsverfahren"
 	"github.com/shopspring/decimal"
-	"time"
 )
 
 // TestZaehlerstaneLen tests the Zaehlerstaende.Len function
-func (s *Suite) Test_Zaehlerstande_Len() {
+func Test_Zaehlerstande_Len(t *testing.T) {
 	zaehlerstaende3 := com.Zaehlerstaende{
 		com.Zaehlerstand{},
 		com.Zaehlerstand{},
 		com.Zaehlerstand{},
 	}
 	zaehlerstaende0 := com.Zaehlerstaende{}
-	then.AssertThat(s.T(), zaehlerstaende3.Len(), is.EqualTo(3))
-	then.AssertThat(s.T(), zaehlerstaende0.Len(), is.EqualTo(0))
+	then.AssertThat(t, zaehlerstaende3.Len(), is.EqualTo(3))
+	then.AssertThat(t, zaehlerstaende0.Len(), is.EqualTo(0))
 	zaehlerstaende0 = nil
-	then.AssertThat(s.T(), zaehlerstaende0.Len(), is.EqualTo(0))
+	then.AssertThat(t, zaehlerstaende0.Len(), is.EqualTo(0))
 }
 
 // Test_Zaehlerstande_Swap tests the Zaehlerstaende.Swap function
-func (s *Suite) Test_Zaehlerstande_Swap() {
+func Test_Zaehlerstande_Swap(t *testing.T) {
 	zaehlerstaende := com.Zaehlerstaende{
 		com.Zaehlerstand{
 			Ablesedatum:              time.Time{},
@@ -46,15 +48,15 @@ func (s *Suite) Test_Zaehlerstande_Swap() {
 			Einheit:                  mengeneinheit.KW,
 		},
 	}
-	then.AssertThat(s.T(), zaehlerstaende[1].Wert, is.EqualTo(decimal.NewFromFloat(42.1)))
-	then.AssertThat(s.T(), zaehlerstaende[2].Wert, is.EqualTo(decimal.NewFromFloat(123.2)))
+	then.AssertThat(t, zaehlerstaende[1].Wert, is.EqualTo(decimal.NewFromFloat(42.1)))
+	then.AssertThat(t, zaehlerstaende[2].Wert, is.EqualTo(decimal.NewFromFloat(123.2)))
 	zaehlerstaende.Swap(1, 2)
-	then.AssertThat(s.T(), zaehlerstaende[1].Wert, is.EqualTo(decimal.NewFromFloat(123.2)))
-	then.AssertThat(s.T(), zaehlerstaende[2].Wert, is.EqualTo(decimal.NewFromFloat(42.1)))
+	then.AssertThat(t, zaehlerstaende[1].Wert, is.EqualTo(decimal.NewFromFloat(123.2)))
+	then.AssertThat(t, zaehlerstaende[2].Wert, is.EqualTo(decimal.NewFromFloat(42.1)))
 }
 
 // Test_Zaehlerstaende_Less tests the Zaehlerstaende.Less function
-func (s *Suite) Test_Zaehlerstaende_Less() {
+func Test_Zaehlerstaende_Less(t *testing.T) {
 	zaehlerstaende := com.Zaehlerstaende{
 		com.Zaehlerstand{
 			Ablesedatum:              time.Date(2025, 8, 1, 0, 0, 0, 0, time.UTC),
@@ -87,10 +89,10 @@ func (s *Suite) Test_Zaehlerstaende_Less() {
 			Einheit:                  mengeneinheit.KW,
 		},
 	}
-	then.AssertThat(s.T(), zaehlerstaende.Less(1, 2), is.True())
-	then.AssertThat(s.T(), zaehlerstaende.Less(2, 1), is.False())
-	then.AssertThat(s.T(), zaehlerstaende.Less(3, 0), is.False())
-	then.AssertThat(s.T(), zaehlerstaende.Less(0, 3), is.False())
-	then.AssertThat(s.T(), zaehlerstaende.Less(0, 4), is.True())
-	then.AssertThat(s.T(), zaehlerstaende.Less(4, 0), is.False())
+	then.AssertThat(t, zaehlerstaende.Less(1, 2), is.True())
+	then.AssertThat(t, zaehlerstaende.Less(2, 1), is.False())
+	then.AssertThat(t, zaehlerstaende.Less(3, 0), is.False())
+	then.AssertThat(t, zaehlerstaende.Less(0, 3), is.False())
+	then.AssertThat(t, zaehlerstaende.Less(0, 4), is.True())
+	then.AssertThat(t, zaehlerstaende.Less(4, 0), is.False())
 }
