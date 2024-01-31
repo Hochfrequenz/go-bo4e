6bba80521acce19eb398df89a751f3f95dc172e5
larisa <larisa.kosareva@hochfrequenz.de>
2024-01-31 12:19:50 +0100

    smth working

--- generate-go-structs.go
+++ generate-go-structs.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 )
 
-func runQuicktype(jsonPath, outputFile string) error {
-	cmd := exec.Command("quicktype", "--lang", "go", "--src", jsonPath, "--out", outputFile)
+func runQuicktype(jsonPath, outputDir string) error {
+	cmd := exec.Command("quicktype", "--src", jsonPath, "-o", outputDir, "--src-lang", "schema", "--lang", "go", "--multi-file-output")
+
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("error running quicktype: %s\nOutput:\n%s", err, output)
@@ -17,28 +19,70 @@ func runQuicktype(jsonPath, outputFile string) error {
 	return nil
 }
 
+func copyGoFileToDestination(temporaryPath, destinationPath, jsonFileName string) error {
+	// Assuming that the generated Go file has the same name as the JSON file
+	goFileName := jsonFileName + ".go"
+
+	// Create the destination directory if it doesn't exist
+	if err := os.MkdirAll(destinationPath, os.ModePerm); err != nil {
+		return err
+	}
+
+	// Calculate paths
+	temporaryFilePath := filepath.Join(temporaryPath, goFileName)
+	destinationFilePath := filepath.Join(destinationPath, goFileName)
+
+	// Copy the Go file to the destination
+	if err := copyFile(temporaryFilePath, destinationFilePath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Copy file function
+func copyFile(src, dest string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	return destFile.Sync()
+}
+
 func main() {
-	jsonPath := "./bo4e_schemas/bo/Angebot.json"
-	temporaryDestination := "./temp"
+	jsonPath := "bo4e_schemas/bo/Angebot.json"
+	temporaryDestination := "temp/bo/Angebot.go"
+	finalDestination := "destination"
 
 	// Extract the file name without extension
 	jsonFileName := strings.TrimSuffix(filepath.Base(jsonPath), filepath.Ext(jsonPath))
 
-	// Create temporary directory if it doesn't exist
-	if err := os.MkdirAll(temporaryDestination, os.ModePerm); err != nil {
-		fmt.Println("Error creating temporary directory:", err)
-		return
-	}
-
-	// Specify the output file path
-	outputFilePath := filepath.Join(temporaryDestination, fmt.Sprintf("%s.go", jsonFileName))
-
 	// Run quicktype
-	err := runQuicktype(jsonPath, outputFilePath)
+	err := runQuicktype(jsonPath, temporaryDestination)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Printf("Go file generated successfully for %s\n", jsonFileName)
+	// Copy the generated Go file to the final destination
+	err = copyGoFileToDestination(temporaryDestination, finalDestination, jsonFileName)
+	if err != nil {
+		fmt.Println("Error copying Go file to destination:", err)
+		return
+	}
+
+	fmt.Printf("Go files generated and copied successfully for %s\n", jsonFileName)
 }
