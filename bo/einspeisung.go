cbd51301894fefc608701594fa28b27b52ab3e84
Franziska <franziska.vesely@hochfrequenz.de>
2024-08-19 18:06:29 +0200

    Change json spelling to dotnet-bo4e version

--- bo/einspeisung.go
+++ bo/einspeisung.go
@@ -1,7 +1,7 @@
 package bo
 
 import (
-	eegvermarktungsform "github.com/hochfrequenz/go-bo4e/enum/eegvermarktungsform"
+	"github.com/hochfrequenz/go-bo4e/enum/eegvermarktungsform"
 	"github.com/hochfrequenz/go-bo4e/enum/fernsteuerbarkeitstatus"
 	"github.com/hochfrequenz/go-bo4e/enum/geschaeftspartnerrolle"
 	"github.com/hochfrequenz/go-bo4e/enum/landescode"
@@ -13,7 +13,7 @@ type Einspeisung struct {
 	MarktlokationsId        string                                          `json:"marktlokationsId,omitempty"`        // Für welche Marktlokation gelten diese Einspeisedaten
 	TrancheId               string                                          `json:"trancheId,omitempty"`               // Für welche Tranche gelten diese Einspeisedaten
 	Verguetungsempfaenger   geschaeftspartnerrolle.Geschaeftspartnerrolle   `json:"verguetungsempfaenger,omitempty"`   // Empfänger der Vergütung zur Einspeisung
-	EEGVermarktungsform     eegvermarktungsform.EEGVermarktungsform         `json:"eEGVermarktungsform,omitempty"`     // Vermarktungsformen gemäß dem Erneuerbare-Energien-Gesetz (EEG).
+	EEGVermarktungsform     eegvermarktungsform.EEGVermarktungsform         `json:"eegVermarktungsform,omitempty"`     // Vermarktungsformen gemäß dem Erneuerbare-Energien-Gesetz (EEG).
 	Landescode              *landescode.Landescode                          `json:"landescode,omitempty"`              // Land der Förderung
 	FernsteuerbarkeitStatus fernsteuerbarkeitstatus.FernsteuerbarkeitStatus `json:"fernsteuerbarkeitStatus,omitempty"` // Status der Fernsteuerbarkeit einer Marktlokation
 }
