e4becee5f4c1d908c4f82776c63862d7546f99f7
Franziska <franziska.vesely@hochfrequenz.de>
2024-08-19 18:07:05 +0200

    Comment problematic BO Marktteilnehmer in COM Konfigurationsprodukt

--- com/konfigurationsprodukt.go
+++ com/konfigurationsprodukt.go
@@ -1,13 +1,10 @@
 package com
 
-import (
-	"github.com/hochfrequenz/go-bo4e/bo"
-)
-
 // Konfigurationsprodukt TODO explain what this is
 type Konfigurationsprodukt struct {
-	Produktcode               string             `json:"produktcode,omitempty"`               // Der Konfigurationsprodukt-Code für das Objekt
-	Leistungskurvendefinition string             `json:"leistungskurvendefinition,omitempty"` // Code der zugeordneten Leistungskurvendefinition für das Objekt
-	Schaltzeitdefinition      string             `json:"schaltzeitdefinition,omitempty"`      // Code der zugeordneten Schaltzeitdefinition für das Objekt
-	Marktpartner              bo.Marktteilnehmer `json:"marktpartner,omitempty"`              //Auftraggebender Marktpartner
+	Produktcode               string `json:"produktcode,omitempty"`               // Der Konfigurationsprodukt-Code für das Objekt
+	Leistungskurvendefinition string `json:"leistungskurvendefinition,omitempty"` // Code der zugeordneten Leistungskurvendefinition für das Objekt
+	Schaltzeitdefinition      string `json:"schaltzeitdefinition,omitempty"`      // Code der zugeordneten Schaltzeitdefinition für das Objekt
+	// Unfortunately adding the Marktteilnehmer here causes a serious issue: "could not import github.com/hochfrequenz/go-bo4e/bo (-: import cycle not allowed in test) (typecheck)". I comment it until we find a good solution
+	// Marktpartner              bo.Marktteilnehmer `json:"marktpartner,omitempty"`              //Auftraggebender Marktpartner
 }
