59cab0ede11f4177a5cef142708b4afa53abee76
Franziska <73471037+hf-fvesely@users.noreply.github.com>
2024-08-21 23:38:20 +0200

    Add a lot of formatversions
    Co-authored-by: konstantin <konstantin.klein@hochfrequenz.de>

--- market_communication/boney_comb_test.go
+++ market_communication/boney_comb_test.go
@@ -217,7 +217,7 @@ func getJsonFilePathsFromSubmodule(dir string) ([]string, error) {
 	return jsonFiles, nil
 }
 
-var formatAllowList = []string{"UTILMD", "MSCONS", "INVOIC", "COMDIS", "PRICAT", "QUOTES"}
+var formatAllowList = []string{"REMADV", "UTILMD", "MSCONS", "INVOIC", "COMDIS", "PRICAT", "QUOTES"}
 var formatVersionAllowList = []string{"FV2210", "FV2310", "FV2404"}
 
 func matchesAllowLists(fileName string) bool {
