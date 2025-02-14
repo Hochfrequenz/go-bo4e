6c9a2c564641f380ea9966e5dbcb63c3519cedee
konstantin <konstantin.klein@hochfrequenz.de>
2025-02-14 10:42:44 +0100

    feat: Add Energierichtung `RUHEND` and `KUNDENANLAGE` (#354)
    https://github.com/Hochfrequenz/BO4E-dotnet/blob/a0d593fa870b4310d55946320306b90354f5842e/BO4E/ENUM/Energierichtung.cs#L20-L28
    
    fixes #353
    
    Co-authored-by: Konstantin <konstantin.klein+github@hochfrequenz.de>

--- enum/energierichtung/energierichtung.go
+++ enum/energierichtung/energierichtung.go
@@ -8,6 +8,9 @@ type Energierichtung int
 
 const (
 	// AUSSP is short for "Ausspeisung" / supply
-	AUSSP Energierichtung = iota + 1
-	EINSP                 // EINSP is short for "Einspeisung" / feeding
+	AUSSP  Energierichtung = iota + 1
+	EINSP                  // EINSP is short for "Einspeisung" / feeding
+	RUHEND                 // RUHEND e Lokation (in einer Kundenanlage)
+
+	KUNDENANLAGE // KUNDENANLAGE kennzeichnet eine Marktlokation als Kundenanlage gemäß EnWG
 )
