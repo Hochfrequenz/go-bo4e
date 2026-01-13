434aa32d246b9a0da0abe6303e204a9662bf68b4
konstantin <konstantin.klein@hochfrequenz.de>
2026-01-13 10:16:53 +0100

    fix(com): align Hardware.GeraeteTyp json tag with C# JsonPropertyName (#449)

--- com/hardware.go
+++ com/hardware.go
@@ -4,6 +4,6 @@ import "github.com/hochfrequenz/go-bo4e/enum/geraetetyp"
 
 // A Hardware is a billable device
 type Hardware struct {
-	GeraeteTyp  *geraetetyp.Geraetetyp `json:"geraeteTyp,omitempty"`                          // GeraeteTyp type of the hardware
+	GeraeteTyp  *geraetetyp.Geraetetyp `json:"geraetetyp,omitempty"`                          // GeraeteTyp type of the hardware
 	Bezeichnung *string                `json:"bezeichnung,omitempty" validate:"alphaunicode"` // Bezeichnung is a description
 }
